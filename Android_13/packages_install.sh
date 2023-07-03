e991170efc83e2e1a3c5ab748c7e29dfe4a3aceb
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-07-03 18:35:14 +0200

    Android_13 init script - sort packages

--- Android_13/packages_install.sh
+++ Android_13/packages_install.sh
@@ -8,16 +8,16 @@ pkg upgrade
 pkg install x11-repo
 # TODO setup termux X11 see https://github.com/termux/termux-x11
 
-pkg install man
-pkg install neovim
 pkg install build-essential
 pkg install git
-pkg install python
-pkg install openssh
-pkg install ripgrep
-pkg install tree
-pkg install neofetch
 pkg install htop
+pkg install man
+pkg install neofetch
+pkg install neovim
+pkg install openssh
+pkg install python
+pkg install ripgrep
 pkg install tmux
+pkg install tree
 
 
