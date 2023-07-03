f9cefa330244bd6bbb443b8e5a77d735065f0cd5
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-07-03 12:21:43 +0200

    Hide my email address.

--- SharedConfigs/git_and_mkdirs.ps1
+++ SharedConfigs/git_and_mkdirs.ps1
@@ -7,5 +7,5 @@ mkdir ~/x/tmp # stuff that can be deleted after restart
 git config --global core.editor "nvim"
 git config --global user.name "JohnnyRevay"
 # TODO make the email a bit more SPAM resistant as it is in a public repo...
-git config --global user.email "janci.revay@gmail.com"
+git config --global user.email "65253638+jan-revay@users.noreply.github.com"
 git config --list --show-origin
