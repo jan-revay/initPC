9575f167a2a3b36cf0323a9e97697b898e23f1f0
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-07-04 23:24:56 +0200

    WSL_Ubuntu_22.04 - rewrite package list as array

--- WSL_Ubuntu_22.04/packages_install.sh
+++ WSL_Ubuntu_22.04/packages_install.sh
@@ -21,23 +21,19 @@ rm llvm.sh
 popd || exit
 
 # see https://github.com/rr-debugger/rr/wiki/Using-rr-in-an-IDE
-APT_PACKAGES="build-essential rr ccache ninja-build cmake cmake-gui cmake-format" # GCC and build tools
+APT_PACKAGES=(build-essential rr ccache ninja-build cmake cmake-gui) # GCC and build tools
 # APT_PACKAGES+=" linux-tools-$(uname -r)" # TODO broken
-
-APT_PACKAGES+=" neovim emacs qtcreator" # editors
-APT_PACKAGES+=" ripgrep tree curl neofetch htop tmux at zsh traceroute jq" # utils1
-APT_PACKAGES+=" dconf-editor doxygen git" # utils2
-APT_PACKAGES+=" python3-pip" # various runtimes
+APT_PACKAGES+=(neovim emacs qtcreator) # editors
+APT_PACKAGES+=(ripgrep tree curl neofetch htop tmux at zsh traceroute jq) # utils1
+APT_PACKAGES+=(dconf-editor doxygen git) # utils2
+APT_PACKAGES+=(python3-pip) # various runtimes
 # TODO maybe add default-jre and dotnet7?
-
-APT_PACKAGES+=" cppcheck cppcheck-gui iwyu clazy" # Static analyzers
-
-APT_PACKAGES+=" valgrind" # Dynamic analyzers
-
-# Python packages
-APT_PACKAGES+=" python3-matplotlib python3-mock python3-numpy python3-pandas"
-APT_PACKAGES+=" python3-pytest python3-requests python3-scipy"
-sudo apt install -y ${APT_PACKAGES}
+APT_PACKAGES+=(cppcheck cppcheck-gui iwyu clazy) # Static analyzers
+APT_PACKAGES+=(cmake-format shfmt) # Code formatters
+APT_PACKAGES+=(valgrind) # Dynamic analyzers
+APT_PACKAGES+=(python3-matplotlib python3-mock python3-numpy python3-pandas) # Python packages
+APT_PACKAGES+=(python3-pytest python3-requests python3-scipy) # Python packages
+sudo apt install -y "${APT_PACKAGES[@]}"
 
 # cleanup
 sudo apt autoremove
