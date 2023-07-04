df8d32f5582f7967f578d31564b3c2d9b9232c22
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-07-04 23:52:06 +0200

    WSL_Ubuntu_22.04 - add a few apt packets (dynamic analyzers)

--- WSL_Ubuntu_22.04/packages_install.sh
+++ WSL_Ubuntu_22.04/packages_install.sh
@@ -30,7 +30,7 @@ APT_PACKAGES+=(python3-pip) # various runtimes
 # TODO maybe add default-jre and dotnet7?
 APT_PACKAGES+=(cppcheck cppcheck-gui iwyu clazy) # Static analyzers
 APT_PACKAGES+=(cmake-format shfmt) # Code formatters
-APT_PACKAGES+=(valgrind) # Dynamic analyzers
+APT_PACKAGES+=(valgrind hotspot heaptrack) # Dynamic analyzers , TODO test them
 APT_PACKAGES+=(python3-matplotlib python3-mock python3-numpy python3-pandas) # Python packages
 APT_PACKAGES+=(python3-pytest python3-requests python3-scipy) # Python packages
 sudo apt install -y "${APT_PACKAGES[@]}"
