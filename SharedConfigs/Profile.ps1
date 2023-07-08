b44a589ebf1ac0d174ef77f1b8a2f86b0d0b45c3
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-07-08 15:33:19 +0200

    Windows 10 - Powershell aliases stub

---
+++ SharedConfigs/Profile.ps1
@@ -0,0 +1,58 @@
+# 1-3 letter aliases
+# free one letter aliases(9): ijknoquwy
+# function a {git add}
+function a. {git add .}
+# function b {git branch}
+function c {git commit}
+function ca {git commit --amend --no-edit}
+# function ch {git checkout}
+function d {git diff}
+# function e {echo}
+# function f {find}
+function gl {git log}
+# function gc {git clone}
+# function gg {git grep}
+# function h {history} - already exists
+# function l {ls -CF}
+# function L {ls -A}
+# function ll {ls -alF}
+# function m {help}
+function ph {git push}
+function pl {git pull}
+# function py {python3}
+# function r {git reset}  - already exists
+function r. {git reset HEAD~1}
+function r.. {git reset HEAD~2}
+function s {git status}
+function sph {git stash push}
+function spo {git stash pop}
+# function t {cat}
+New-Alias -Name v -Value nvim
+function x {Set-Location ~/x/} # all my development repos go her
+
+
+# Set-Alias -Name a -Value a
+# Set-Alias -Name b -Value b
+# Set-Alias -Name c -Value c
+# Set-Alias -Name d -Value d
+# Set-Alias -Name e -Value e
+# Set-Alias -Name f -Value f
+# Set-Alias -Name g -Value g
+# Set-Alias -Name h -Value h
+# Set-Alias -Name i -Value i
+# Set-Alias -Name j -Value j
+# Set-Alias -Name k -Value k
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
+# Set-Alias -Name a -Value a
--- Windows_10/configs_install.ps1
+++ Windows_10/configs_install.ps1
@@ -1,4 +1,5 @@
 # TODO:
+# RUN AS ADMINISTRATOR
 # scaling on 4k monitor - try avoiding the fractional scaling as it causes blur sometimes
 # TODO windows registry stuff
 # TODO nvim config stuff
@@ -6,6 +7,8 @@
 
 ../SharedConfigs/git_config.sh.ps1
 ../SharedConfigs/mkdirs.ps1
+
+Copy-Item ../SharedConfigs/Profile.ps1 $PSHOME\Profile.ps1
 
 # Manually changed settings
 # Todo try to automatically set these windows settings in future
