fd94557c5d1e3b61a8424bb9d41e1366c473f734
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-07-09 17:18:36 +0200

    Add PowerShell aliases

--- SharedConfigs/Profile.ps1
+++ SharedConfigs/Profile.ps1
@@ -1,63 +1,57 @@
 # 1-3 letter aliases
 # free one letter aliases(9): ijknoquwy
-# function a {git add}
-function a. {git add .}
-# function b {git branch}
-function c {git commit}
-function ca {git commit --amend --no-edit}
-# function ch {git checkout}
-function d {git diff}
-# function e {echo}
+function a { & git add $args }
+function a. { & git add . $args }
+function aa { & git add --all $args } # 'A' on Unix
+function b { & git branch $args }
+function c { & git commit $args }
+function ca { & git commit --amend --no-edit $args }
+function ch { & git checkout $args }
+function d { & git diff $args }
+New-Alias -Name e -Value echo
 # function f {find}
-function gl {git log}
-# function gc {git clone}
-# function gg {git grep}
-# function h {history} - already exists
-# function l {ls -CF}
-# function L {ls -A}
-# function ll {ls -alF}
-# function m {help}
-function ph {git push}
-function pl {git pull}
-# function py {python3}
-# function r {git reset}  - already exists
-function r. {git reset HEAD~1}
-function r.. {git reset HEAD~2}
-function s {git status}
-function sph {git stash push}
-function spo {git stash pop}
-# function t {cat}
+function g { & git $args }
+function gl { & git log --stat $args }
+function glo { & git log --oneline --decorate --color $args }
+function gc { & git clone $args }
+function gg { & git grep $args }
+# function l { & Get-ChildItem -CF $args }
+# function L { & Get-ChildItem -A $args }
+# function ll { & Get-ChildItem -alF $args }
+function m { & help $args }
+function ph { & git push $args }
+function pl { & git pull $args }
+
+# function py {python3} - this already exists on Windows
+# function r {git reset}  - already exists in Powershell as some other command TODO
+function r. { & git reset $args HEAD~1 }
+function r.. { & git reset $args HEAD~2 }
+function s { & git status $args }
+function ss { & git status --short $args }
+function sph { & git stash push $args }
+function spo { & git stash pop $args }
+New-Alias -Name t -Value cat
 New-Alias -Name v -Value nvim
-function x {Set-Location ~/x/} # all my development repos go her
+function x { Set-Location ~/x/ } # all my development repos go her
 
 
-# Set-Alias -Name a -Value a
-# Set-Alias -Name b -Value b
-# Set-Alias -Name c -Value c
-# Set-Alias -Name d -Value d
-# Set-Alias -Name e -Value e
-# Set-Alias -Name f -Value f
-# Set-Alias -Name g -Value g
-# Set-Alias -Name h -Value h
-# Set-Alias -Name i -Value i
-# Set-Alias -Name j -Value j
-# Set-Alias -Name k -Value k
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
-# Set-Alias -Name a -Value a
+# alias cd..='cd ..' - already exists in PowerShell
+function .. { Set-Location .. }
+function ... { Set-Location ../../.. }
+function .... { Set-Location ../../../.. }
+function ..... { Set-Location ../../../../.. }
 
-# TODO
+function cd1 { Set-Location .. }
+function cd2 { Set-Location ../.. }
+function cd3 { Set-Location ../../.. }
+function cd4 { Set-Location ../../../.. }
+function cd5 { Set-Location ../../../../.. }
+function cd6 { Set-Location ../../../../../.. }
+function cd7 { Set-Location ../../../../../../.. }
+function cd8 { Set-Location ../../../../../../../.. }
+function cd9 { Set-Location ../../../../../../../../.. }
+
+# TODO READ
 # Liniks:
 # https://stackoverflow.com/questions/55539278/is-there-a-way-to-create-an-alias-to-a-cmdlet-in-a-way-that-it-only-runs-if-argu/55539863#55539863
 # https://learn.microsoft.com/en-us/powershell/scripting/developer/cmdlet/approved-verbs-for-windows-powershell-commands?view=powershell-7.3
