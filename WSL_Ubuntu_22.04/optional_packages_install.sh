527784cf8d6a0fb149b3ad664ed48a896677d481
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-14 01:21:54 +0200

    Update optional_packages_install.sh

--- WSL_Ubuntu_22.04/optional_packages_install.sh
+++ WSL_Ubuntu_22.04/optional_packages_install.sh
@@ -3,7 +3,7 @@
 # The script should not require any user input and should be idempotent.
 
 # TODO add:
-#    - zig compiler
+#    - zig compiler and language server
 #    - rust compiler
 #    - ICC and AOCC (Intel and AMD C++ compilers)
 # add foliate: sudo apt install foliate
