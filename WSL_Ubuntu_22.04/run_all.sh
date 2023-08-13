1b677eae01ee21bc7d4dc8f05fb99740a90f39fe
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-13 17:49:02 +0200

    run_all.sh - bump Ubuntu version

--- WSL_Ubuntu_22.04/run_all.sh
+++ WSL_Ubuntu_22.04/run_all.sh
@@ -6,8 +6,8 @@
 export PS4="\[\033[1;93m\]+ \[\033[0m\]"
 set -e # exit on error
 
-if [[ "$(lsb_release --description --short)" != 'Ubuntu 22.04.2 LTS' ]]; then
-    echo 'Error: The base image does not match "Ubuntu 22.04.2 LTS"! Aborting.'
+if [[ "$(lsb_release --description --short)" != 'Ubuntu 22.04.3 LTS' ]]; then
+    echo 'Error: The base image does not match "Ubuntu 22.04.3 LTS"! Aborting.'
     exit 1
 fi
 
