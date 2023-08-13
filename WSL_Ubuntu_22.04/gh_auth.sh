cc811f348ab1f8d6313e96431c21c8e62f83d0b0
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-13 20:28:54 +0200

    WSL_Ubuntu - gh_auth.sh - don't prompt if logged in

--- WSL_Ubuntu_22.04/gh_auth.sh
+++ WSL_Ubuntu_22.04/gh_auth.sh
@@ -3,19 +3,21 @@
 # BASE IMAGE: WSL Ubuntu 22.04
 # REQUIRES A USER INPUT
 
-read -sp 'Enter GitHub authentication token: ' TOKEN
-readonly TOKEN
+if ! gh auth status; then
+    read -sp 'Enter GitHub authentication token: ' TOKEN
+    readonly TOKEN
 
-# write directly because `gh auth login --with-token` is broken
-echo \
-"github.com:
-    oauth_token: "${TOKEN}"
-    user: jan-revay
-    git_protocol: https" \
-> /home/jr/.config/gh/hosts.yml
+    # write directly because `gh auth login --with-token` is broken
+    echo \
+    "github.com:
+        oauth_token: "${TOKEN}"
+        user: jan-revay
+        git_protocol: https" \
+    > /home/jr/.config/gh/hosts.yml
 
-# cmd bellow is not working - it prompts for username and pw anyway
-# echo "${TOKEN}" | gh auth login --with-token
-# gh config set -h github.com git_protocol https # -- probably not needed
-echo
-echo Logged in to guthub.com
+    # cmd bellow is not working - it prompts for username and pw anyway
+    # echo "${TOKEN}" | gh auth login --with-token
+    # gh config set -h github.com git_protocol https # -- probably not needed
+    echo
+    echo Logged in to guthub.com
+fi
