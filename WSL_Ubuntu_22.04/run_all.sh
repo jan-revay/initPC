157dfafc72484788fb209b7ed9d7c0ca6ab85466
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-13 21:36:14 +0200

    WSL_Ubuntu - run_all.sh - don't check for subversion

--- WSL_Ubuntu_22.04/run_all.sh
+++ WSL_Ubuntu_22.04/run_all.sh
@@ -6,8 +6,8 @@
 export PS4="\[\033[1;93m\]+ \[\033[0m\]"
 set -e # exit on error
 
-if [[ "$(lsb_release --description --short)" != 'Ubuntu 22.04.3 LTS' ]]; then
-    echo 'Error: The base image does not match "Ubuntu 22.04.3 LTS"! Aborting.'
+if [[ "$(lsb_release --description --short)" != 'Ubuntu 22.04'* ]]; then
+    echo 'Error: The base image does not match "Ubuntu 22.04"! Aborting.'
     exit 1
 fi
 
