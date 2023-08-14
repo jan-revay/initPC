8162f9140c4a83c956eaf93e7f5270b8db4c506d
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-14 16:39:25 +0200

    run_all.ps1 - run interactive scripts first

--- Windows_10/run_all.ps1
+++ Windows_10/run_all.ps1
@@ -1,9 +1,12 @@
 # TODO test
 Set-PSDebug -Trace 1
 
+# Scripts requiring user input
+. $PSScriptRoot/wsl_install.ps1
+
+# Noninteractive scripts
 . $PSScriptRoot/packages_install.ps1
 # ./packages__install_optional.ps1
-. $PSScriptRoot/wsl_install.ps1
 . $PSScriptRoot/configs_install.ps1
 
 Set-PSDebug -Trace 0
