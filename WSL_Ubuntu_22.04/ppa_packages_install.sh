5f9671680ea0af07edbe734a960496ff12c8f78c
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-15 01:45:06 +0200

    Delete ppa_packages_install.sh

--- WSL_Ubuntu_22.04/ppa_packages_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-# BASE IMAGE: WSL Ubuntu 22.04
-# The script should not require any user input and should be idempotent.
-
-sudo add-apt-repository -y ppa:maveonair/helix-editor
-sudo apt-get update
-
-APT_PACKAGES=(helix)
-sudo apt-get install -y "${APT_PACKAGES[@]}"
