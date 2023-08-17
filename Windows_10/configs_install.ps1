fb0c8c29eb2da6852cc3c0bbf1ac6494ebf5dad1
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-17 23:25:47 +0200

    Prepare dotfiles to be moved out of repo

--- Windows_10/configs_install.ps1
+++ Windows_10/configs_install.ps1
@@ -7,12 +7,12 @@
 
 Set-PSDebug -Trace 1
 
-. $PSScriptRoot/../Dotfiles/git_config.sh.ps1
-. $PSScriptRoot/../Dotfiles/mkdirs.ps1
+. $PSScriptRoot/../CommonInitScripts/git_config.sh.ps1
+. $PSScriptRoot/../CommonInitScripts/mkdirs.ps1
 
 # Todo consider adding this only for current user so that I don't need sudo.
 # Maybe that would also be safer.
-Copy-Item $PSScriptRoot/../Dotfiles/Profile.ps1 $PSHOME/Profile.ps1
+Copy-Item $PSScriptRoot/win_dotfiles/Profile.ps1 $PSHOME/Profile.ps1
 
 # Manually changed settings
 # Todo try to automatically set these windows settings in future
