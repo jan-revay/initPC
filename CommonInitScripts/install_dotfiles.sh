2e31d4fea9742af1cc9487ca2b239a547d8d9fda
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-19 00:37:01 +0200

    install_dotfiles.sh - first version that works

--- CommonInitScripts/install_dotfiles.sh
+++ CommonInitScripts/install_dotfiles.sh
@@ -6,41 +6,49 @@
 # makes the echo prompt yellow to improve readability
 export PS4="\[\033[1;93m\]+ \[\033[0m\]"
 set -e # exit on error
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
+# TODO replace tabs with spaces
 # Using bare git repo for management of config files
 # see: https://www.atlassian.com/git/tutorials/dotfiles
-BRANCH=$(git symbolic-ref --short HEAD)
-readonly BRANCH
 function dot {
     /usr/bin/git --git-dir="${HOME}/.dotfiles/" --work-tree="${HOME}" $@
 }
 
-function backup_dotfiles {
-    mkdir -p ~/.dotfiles_automatic_backup/
-    OLD_DOTFILES=$(dot checkout 2>&1 | grep -E "\s+\." | awk {'print $1'})
+# TODO Make .dotfiles_auto_backup a git repo and commit on every new backup (maybe I could even push somewhere upstream)
+function backup_conflicting_dotfiles {
+    echo -e "${GREEN}Backing up old dotfiles${NC}"
+    mkdir -p ~/.dotfiles_auto_backup/
+    OLD_DOTFILES=$(dot checkout 2>&1 | grep "^[[:space:]][^[:space:]]\+$" | awk {'print $1'})
     readonly OLD_DOTFILES
 
-    echo "$OLD_DOTFILES" |
-        xargs -I{} mkdir "${HOME}/.dotfiles_automatic_backup/$(dirname {})"
-    echo "$OLD_DOTFILES" |
-        xargs -I{} mv ~/{} ~/.dotfiles_automatic_backup/{}
+    OLD_DOTDIRS=$(echo "$OLD_DOTFILES" | xargs -I{} dirname ${HOME}/.dotfiles_auto_backup/{})
+    echo "$OLD_DOTDIRS" | xargs -I{} mkdir -p {}
+    echo "$OLD_DOTFILES" | xargs -I{} mv ~/{} ~/.dotfiles_auto_backup/{}
 }
 
+function checkout_dotfiles {
+    BRANCH=$(git symbolic-ref --short HEAD)
+    readonly BRANCH
+    if ! dot checkout "${BRANCH}"; then
+        backup_conflicting_dotfiles
+        dot checkout "${BRANCH}"
+    fi
+
+    echo -e "${GREEN}Checked out dotfiles branch:" ${BRANCH}${NC}
+}
+# TODO merge similar stuff in the two branches of the if stmnt
+# TODO consider using git checkout --force as a simplification
 if [ ! -d ~/.dotfiles ]; then
     echo ".dotfiles" > ~/.gitignore
     # echo "*" > ~/.gitignore  # secure option
-    git clone --bare https://github.com/jan-revay/dotfiles.git \
-        "${HOME}/.dotfiles" --branch "${BRANCH}"
-
-    # TODO improve not to filter just the dot
-    backup_dotfiles
-    dot checkout "${BRANCH}"
-    dot config --local status.showUntrackedFiles no
-else
-    dot pull
-    backup_dotfiles
-    dot checkout "${BRANCH}"
+    git clone --bare https://github.com/jan-revay/dotfiles.git "${HOME}/.dotfiles"
 fi
+
+checkout_dotfiles
+dot config --local status.showUntrackedFiles no
+dot pull
 
 # TODO consider having git tracked .bashrc (that way I don't need to add anything)
 # ==== .bashrc stuff ====
