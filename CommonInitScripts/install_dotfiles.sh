5762726c10d65f6df25f1a3894c038dd72d9a447
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-18 21:52:01 +0200

    Dotfiles - bare git repo setup

--- CommonInitScripts/install_dotfiles.sh
+++ CommonInitScripts/install_dotfiles.sh
@@ -7,14 +7,24 @@
 export PS4="\[\033[1;93m\]+ \[\033[0m\]"
 set -e # exit on error
 
+# Using bare git repo for management of config files
+# see: https://www.atlassian.com/git/tutorials/dotfiles
+BRANCH=$(git symbolic-ref --short HEAD)
+readonly BRANCH
+echo ".dotfiles" > ~/.gitignore
+git clone --bare https://github.com/jan-revay/dotfiles.git "${HOME}/.dotfiles" \
+    --branch "${BRANCH}"
+alias dot='/usr/bin/git --git-dir="${HOME}/.dotfiles/" --work-tree="${HOME}"'
+# echo "*" > ~/.gitignore  # secure option (I would have to use -f with dot add)
+
+# TODO improve not to filter just the dot
 mkdir -p ~/.old_dotfiles/
+dot checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | \
+    xargs -I{} mv {} .old_dotfiles/{}
+dot checkout
+dot config --local status.showUntrackedFiles no
 
-mv ~/.my_bashrc ~/.old_dotfiles/ || true
-mv ~/.bash_aliases ~/.old_dotfiles/ || true
-
-ln -s "$(realpath ../Dotfiles/.my_bashrc)" ~/
-ln -s "$(realpath ../Dotfiles/.bash_aliases)" ~/
-
+# TODO consider having git tracked .bashrc (that way I don't need to add anything)
 # ==== .bashrc stuff ====
 if ! grep my_bashrc ~/.bashrc; then
     echo "
