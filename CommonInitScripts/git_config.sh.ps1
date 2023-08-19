d48b090ab4403c095c82eb817ce0ea3bec670d17
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-19 13:09:41 +0200

    Fix `--no-pager` flag in `git config --list`

--- CommonInitScripts/git_config.sh.ps1
+++ CommonInitScripts/git_config.sh.ps1
@@ -4,4 +4,4 @@
 git config --global core.editor "nvim"
 git config --global user.name "JohnnyRevay"
 git config --global user.email "65253638+jan-revay@users.noreply.github.com"
-git config --list --show-origin --no-pager
+git --no-pager config --list --show-origin 
