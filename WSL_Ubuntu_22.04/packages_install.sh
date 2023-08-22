0d321b9d0e26d2981902416c5bd3203bd9c5ea3b
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-23 01:14:41 +0200

    Add a TODO

--- WSL_Ubuntu_22.04/packages_install.sh
+++ WSL_Ubuntu_22.04/packages_install.sh
@@ -81,6 +81,7 @@ cargo install procs
 sudo npm i -g bash-language-server
 
 # bare dpkg packages
+# TODO - try automatically picking the highest version
 pushd /tmp || exit
 wget https://github.com/o2sh/onefetch/releases/download/2.18.1/onefetch_2.18.1_amd64.deb
 sudo dpkg -i onefetch_2.18.1_amd64.deb
