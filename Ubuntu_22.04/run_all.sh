0479d36c0b2a456058b657d07d4a9b91701f173d
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-24 01:59:02 +0200

    Ubuntu22.04 - Add `gh auth login`

--- Ubuntu_22.04/run_all.sh
+++ Ubuntu_22.04/run_all.sh
@@ -7,7 +7,7 @@ export PS4="\[\033[1;93m\]+ \[\033[0m\]"
 set -e # exit on error
 
 # TODO add Ubuntu version
-if [[ "$(lsb_release --description --short)" != 'Ubuntu 22.04'* ]]; then
+if [[ "$(lsb_release --description --short)" != 'Ubuntu '* ]]; then
     echo 'Error: The base image does not match "Ubuntu 22.04"! Aborting.'
     exit 1
 fi
@@ -16,10 +16,23 @@ if [[ -f "/etc/wsl.conf" ]]; then
     echo "Warning: The base image is WSL, use the WSL Ubuntu installer instead."
 fi
 
+# TODO "Which services should be restarted?" prompt is still present in VM
+export NEEDRESTART_MODE=a
+export NEEDRESTART_SUSPEND=1
+
+# Run stuff that requires user input first
+# WARNING: gh auth login --with-token is somehow broken (git asks for pw anyway)
+if ! gh auth status; then
+    sudo apt update
+    sudo apt install gh
+    gh auth login --hostname github.com
+fi
+
+
 # shellcheck source=/dev/null
 . ./packages_install.sh
 # shellcheck source=/dev/null
-. ./ppa_packages_install.sh
+. ../WSL_Ubuntu_22.04/ppa_packages_install.sh
 # shellcheck source=/dev/null
 . ./configs_install.sh
 
