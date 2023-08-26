44b9d2b4f1372582d26381e869b7629f47b5f6f8
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-26 20:37:45 +0200

    Add pre-commit script and install instructions

---
+++ pre-commit-hook.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+# TODO precommit hooks WIP
+
+# makes the echo prompt yellow to improve readability
+
+export PS4="\[\033[1;93m\]+ \[\033[0m\]"
+
+set -e # exit on error
+
+find . -type f -name "*.sh" -execdir shfmt -w {} \;
+
+# NOTE: `find -execdir` cannot be used because it does not propagate errors
+# NOTE: I am passing the filename as a parameter to the bash subshell, see
+# https://www.shellcheck.net/wiki/SC2156 for more info
+# shellcheck disable=SC2016
+find . -type f -name "*.sh" -print0 \
+    | xargs -0 -I{} bash -c \
+        'cd $(dirname "$1") &&
+        echo "### Entering directory: $(dirname "$1")" &&
+        shellcheck $(basename "$1")' bash {}
+
+# WARNING: `find -execdir` cannot be used - TODO delete
+# TODO why `find ... -execdir ... {} +` does not work here?
+# I am passing the filename as a parameter to the bash subshell
+# find . -type f -name "*.sh" -execdir \
+#     bash -c 'echo "### Entering directory $(pwd)" && shellcheck "${1}"' bash {} \;
