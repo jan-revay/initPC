1220b9b9731afb6b00ef151f4e062429bb5f6e5b
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-26 17:49:58 +0200

    shellcheck - bugfix parse every file just once
    BUG: shellcheck was parsing some files multiple times

--- precommit_hooks.sh
+++ precommit_hooks.sh
@@ -8,4 +8,4 @@ export PS4="\[\033[1;93m\]+ \[\033[0m\]"
 set -e # exit on error
 
 find . -type f -name "*.sh" | xargs -I{} shfmt -w {}
-find . -type f -name "*.sh" | xargs -I{} bash -c 'cd $(dirname {}) && shellcheck *.sh'
+find . -type f -name "*.sh" | xargs -I{} bash -c 'cd $(dirname {}) && shellcheck $(basename {})'
