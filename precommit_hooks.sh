ae3890b47ed528eeb52488544aab11877a0e0896
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-26 17:46:54 +0200

    shfmt and shellcheck precommit hooks

---
+++ precommit_hooks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+# TODO precommit hooks WIP
+
+# makes the echo prompt yellow to improve readability
+
+export PS4="\[\033[1;93m\]+ \[\033[0m\]"
+
+set -e # exit on error
+
+find . -type f -name "*.sh" | xargs -I{} shfmt -w {}
+find . -type f -name "*.sh" | xargs -I{} bash -c 'cd $(dirname {}) && shellcheck *.sh'
