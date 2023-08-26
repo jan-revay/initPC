6fb4fc033a5cd92015703a48d791791f2d9ea39a
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-26 18:39:01 +0200

    Add shellbang to mkdirs.sh

--- CommonInitScripts/mkdirs.sh
+++ CommonInitScripts/mkdirs.sh
@@ -1,3 +1,11 @@
+#!/bin/bash -x
+# BASE IMAGE: WSL Ubuntu 22.04
+# The script should not require any user input and should be idempotent.
+
+# makes the echo prompt yellow to improve readability
+export PS4="\[\033[1;93m\]+ \[\033[0m\]"
+set -e # exit on error
+
 mkdir -p ~/books
 mkdir -p ~/x     # all my development repos go here
 mkdir -p ~/x/tmp # stuff that can be deleted after restart
