a672f16d4263505235935ae89e05705355163aac
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-26 02:26:20 +0200

    Add a TODO

--- WSL_Ubuntu_22.04/optional_packages_install.sh
+++ WSL_Ubuntu_22.04/optional_packages_install.sh
@@ -7,3 +7,5 @@
 #    - rust compiler
 #    - ICC and AOCC (Intel and AMD C++ compilers)
 # add foliate: sudo apt install foliate
+#    - powershell https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.3
+
