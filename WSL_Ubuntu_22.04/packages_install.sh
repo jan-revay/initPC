6fd45e2c311dfff1e98b612b062a222497eb6264
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-08-29 12:10:39 +0200

    packages_install.sh - don't use `cargo` unless we have to

--- WSL_Ubuntu_22.04/packages_install.sh
+++ WSL_Ubuntu_22.04/packages_install.sh
@@ -79,7 +79,7 @@ source "$HOME/.cargo/env"
 # TODO try to minimize the number of package managers I need to use
 # TODO installing cargo packages is super slow - try to find another way
 # NOTE: du-dust and procs are also available at snap store, tokei is not
-CARGO_PACKAGES=(tokei du-dust procs)
+CARGO_PACKAGES=(tokei)
 time cargo install "${CARGO_PACKAGES[@]}"
 
 # NPM packages
