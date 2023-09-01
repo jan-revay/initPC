7cc8eaf0253584a6d290e45d033fa1bcfcfe6a80
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-09-01 17:34:05 +0200

    run_all.sh - pass exit code and parameters

--- run_init.sh
+++ run_init.sh
@@ -1,39 +1,38 @@
 #!/bin/bash -x
 # BASE IMAGE: all
-# This script tries to detect platform automatically
+# This script tries to automatically detect and init the host platform.
 
 # TODO include this script in CI directly
 
 # makes the echo prompt yellow to improve readability
 export PS4="\[\033[1;93m\]+ \[\033[0m\]"
+set -e # exit on error
 
-pushd .git/hooks/ || exit 40
+pushd .git/hooks/ || exit 50
 ln -s --force ../../pre-commit-hook.sh pre-commit
-popd || exit 40
+popd || exit 50
 
 mkdir -p Logs
 
 LOG_PATH="../Logs/$(date '+%Y%m%d_%H%M%S').log"
-readonly LOG_PATH # the file should be rewritten - TODO rewrite
+readonly LOG_PATH
 
 function try_platform
 {
-    cd "$1" || exit 50
+    pushd "$1" || exit 50
 
-    time ./run_all.sh 2>&1 | tee "${LOG_PATH}"
+    time ./run_all.sh "$2" 2>&1 | tee "${LOG_PATH}"
     # TODO - consider even better error code number (guaranteed to be unique)
     # exit code 126 = incorrect platform
     if [[ "${PIPESTATUS[0]}" != 126 ]]; then
-        exit 0
+        exit "${PIPESTATUS[0]}"
     fi
 
-    cd ..
+    popd || exit 50
 }
 
-try_platform "WSL_Ubuntu_22.04"
-try_platform "WSL_Debian_12"
-try_platform "Ubuntu_22.04"
-try_platform "PopOS_22.04"
-try_platform "Android_13"
-
-
+try_platform "WSL_Ubuntu_22.04" "$1"
+try_platform "WSL_Debian_12" "$1"
+try_platform "Ubuntu_22.04" "$1"
+try_platform "PopOS_22.04" "$1"
+try_platform "Android_13" "$1"
