6a97cbb397e46d141c55cc1eb5a9be65ad4a3aeb
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-09-01 13:46:22 +0200

    configs_install.sh - comment rewrite

--- WSL_Ubuntu_22.04/configs_install.sh
+++ WSL_Ubuntu_22.04/configs_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
-# The script should not require any user input and should be idempotent.
 # BASE IMAGE: WSL Ubuntu 22.04
+# The script should not require any user input and should be idempotent.
 
 # makes the echo prompt yellow to improve readability
 export PS4="\[\033[1;93m\]+ \[\033[0m\]"
