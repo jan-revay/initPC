99b085af0986aba80eb5d7cb48e8ccda7e5b19e8
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-09-17 01:02:08 +0200

    gnome_install_extensions.sh - Add a TODO

--- CommonInitScripts/gnome_install_extensions.sh
+++ CommonInitScripts/gnome_install_extensions.sh
@@ -13,9 +13,10 @@ set -e # exit on error
 # extensions used in Nobara project (linux distro)
 # org.gnome.Shell.Extensions.InstallRemoteExtension() is the same DBus method that is used in the
 # gnome-browser-connector package (the package that installs extesions via Chrome/Firefox plugin)
+# TODO add extensions from this video: https://youtu.be/KtjYPMCvQ7Y?si=sAPoExRbWvSI5g3s
+
 ## @note the gdbus call InstallRemoteExtension() might need to be invoked twice
 ## as sometimes the first call fails.
-
 function install_gnome_extension
 {
     local EXTENSION_ID="$1"
