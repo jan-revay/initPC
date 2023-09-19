e85eb730873ef02b8884f4730899cbe8c2a20309
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-09-19 17:22:09 +0200

    Update interactive_part.sh

--- Ubuntu_22.04/interactive_part.sh
+++ Ubuntu_22.04/interactive_part.sh
@@ -12,6 +12,6 @@
 . ../CommonInitScripts/gnome_install_extensions.sh
 
 # TODO configure postfix in advance from command line
-# TODO what is this app and why does it automatically installs itself on Ubuntu
+# TODO what is this app and why does it automatically install itself on Ubuntu
 # server/desktop, but not in wsl? It needs user interaction hence adding it here.
 sudo apt-get install -y postfix
