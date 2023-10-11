3f8c7b291bf1f83ead8810b322da35e32af4238e
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-10-11 19:25:07 +0200

    Rename similar files

--- Windows_10/run_all.ps1
+++ Windows_10/run_all.ps1
@@ -8,7 +8,8 @@ Set-PSDebug -Trace 1
 
 # Noninteractive scripts
 . $PSScriptRoot/packages_install.ps1
-# ./packages__install_optional.ps1
+# ./optional_packages__install.ps1
 . $PSScriptRoot/configs_install.ps1
+. $PSScriptRoot/manual_packages_install.ps1
 
 Set-PSDebug -Trace 0
