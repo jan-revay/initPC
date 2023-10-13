efa407b1756480f1be79f2b5fce8c48154c73911
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-10-13 10:58:10 +0200

    Update mkdirs.ps1

--- CommonInitScripts/mkdirs.ps1
+++ CommonInitScripts/mkdirs.ps1
@@ -1,3 +1,5 @@
+# TODO - is the -Force parameter really needed here?
+
 mkdir -Force ~/books
 mkdir -Force ~/x # all my development repos go here
 mkdir -Force ~/x/tmp # stuff that can be deleted after restart
