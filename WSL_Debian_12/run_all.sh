0cf3d41e96e62679481a170180cc0eea6f62b083
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-10-15 12:08:26 +0200

    Update run_all.sh

--- WSL_Debian_12/run_all.sh
+++ WSL_Debian_12/run_all.sh
@@ -31,6 +31,7 @@ fi
 
 . ../WSL_Ubuntu_22.04/configs_install.sh
 
-. debian_specific_packages.sh # last, because some packages are built from source
+# TODO - uncomment
+# . debian_specific_packages.sh # last, because some packages are built from source
 
 # . ../WSL_Ubuntu_22.04/packages_install_optional.sh
