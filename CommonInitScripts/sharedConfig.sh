9c539e73caeacf340d49e1a010ef16d44645515c
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2023-10-20 17:29:50 +0200

    Update sharedConfig.sh

--- CommonInitScripts/sharedConfig.sh
+++ CommonInitScripts/sharedConfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 # WARNING: this scripts expects ./CommonInitScripts as a working directory
 
-# TODO rename to snake case shared_config.sh
+# TODO rename to snake case shared_configs_install.sh
 # TODO cd working directory to ./CommonInitScripts in this script
 # see: https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
 
