134ad150edecebe4fb64e48f1451495f84c16a00
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-02-05 22:29:00 +0100

    Update packages_install.sh

--- MacOS/packages_install.sh
+++ MacOS/packages_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -x
 
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/janor/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew install gcc
 brew install 
