97aba2ec28e0c6110801f59eb704eff96fe739a7
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-02-05 22:37:19 +0100

    Update packages_install.sh

--- MacOS/packages_install.sh
+++ MacOS/packages_install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
 
+# Inspiration:
+# - https://github.com/nhoizey/macOS-init/blob/main/run-first.sh
+# https://about.gitlab.com/blog/2020/04/17/dotfiles-document-and-automate-your-macbook-setup/
+# https://gitlab.com/dnsmichi/dotfiles
+# 
+
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/janor/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+brew update
+
 brew install gcc
-brew install 
+brew install todoist
