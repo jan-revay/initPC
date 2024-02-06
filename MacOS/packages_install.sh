c5c765e7701b01fcafe6b6aabf81de57c1949efd
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-02-06 01:34:29 +0100

    Update packages_install.sh

--- MacOS/packages_install.sh
+++ MacOS/packages_install.sh
@@ -15,5 +15,5 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew update
 
 brew install gcc
-brew install todoist logitech-options zoom whatsapp
+brew install todoist logitech-options zoom whatsapp messenger
 brew install alacritty
