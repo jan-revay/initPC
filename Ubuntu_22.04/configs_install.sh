23daee78b7016337f00415575a005f858c1971a7
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-08-02 23:40:02 +0200

    Ubuntu_22.04 - GNOME: Add SK keyboard layout & regroup commands

--- Ubuntu_22.04/configs_install.sh
+++ Ubuntu_22.04/configs_install.sh
@@ -11,7 +11,6 @@
 
 # GNOME TWEAKS
 
-# TODO add lower window on middle click
 # TODO add keyboard settings
 # TODO add fonts settings scaling factor
 # TODO add grup timeout setup to 1
@@ -24,20 +23,26 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
 # GSettings
 # TODO describe the procedure of capturing the GSettings (Dconf monitor)
 gsettings set org.gnome.desktop.input-sources per-window true
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us'), ('xkb', 'sk+qwerty')]"
+gsettings set org.gnome.desktop.input-sources xkb-options \
+    "['terminate:ctrl_alt_bksp', 'shift:both_capslock_cancel', 'caps:none']"
+
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-red-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Yaru-red'
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 8
+
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.4
+gsettings set org.gnome.desktop.interface cursor-size 64
+
 gsettings set org.gnome.desktop.wm.preferences action-middle-click-titlebar 'minimize'
 gsettings set org.gnome.desktop.wm.preferences action-double-click-titlebar 'toggle-maximize'
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:close'
-# todo test the keyboard settings
-gsettings set org.gnome.desktop.input-sources xkb-options \
-    "['terminate:ctrl_alt_bksp', 'shift:both_capslock_cancel', 'caps:none']"
-gsettings set org.gnome.desktop.interface cursor-size 64
+
+# TODO design and add all keybindings
+# Keybindings
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Control><Alt><Super>h']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Control><Alt><Super>j']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Control><Alt><Super>k']"
