52bdbfd11fcccd25102b869637cb425afe396140
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-08-14 21:25:23 +0200

    flatpaks & snaps: Reorder & formatting

--- CommonInitScripts/flatpaks_snaps_appimages_nix.sh
+++ CommonInitScripts/flatpaks_snaps_appimages_nix.sh
@@ -8,8 +8,10 @@
 # TODO VSC flatpak appears to be broken and lower quality than the snap
 # TODO test the flatpaks
 
+# TODO - is this needed?
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+# TODO toread https://man7.org/linux/man-pages/man1/flatpak-install.1.html
 readonly FLATPAK_PACKAGES=(
     com.visualstudio.code
     com.github.johnfactotum.Foliate
@@ -18,16 +20,14 @@ readonly FLATPAK_PACKAGES=(
     com.spotify.Client
     com.axosoft.GitKraken
 )
+time flatpak install --noninteractive --assumeyes flathub "${FLATPAK_PACKAGES[@]}"
 
+# TODO verify Installing snaps from a list is not idempotent for some reason TODO try fixing
+# TODO reloading bashrc might be needed (SNAP)
 readonly SNAP_PACKAGES=(
     core
     snap-store
     todoist
 )
-
-# TODO reloading bashrc might be needed (SNAP)
-# TODO verify Installing snaps from a list is not idempotent for some reason TODO try fixing
 time sudo snap refresh
 time print0 "${SNAP_PACKAGES[@]}" | xargs -0 -I % sudo snap install %
-
-time flatpak install --noninteractive --assumeyes flathub "${FLATPAK_PACKAGES[@]}"
