43d68e2117b538b47fd25d3ce29080c4e780208d
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-08-14 23:17:08 +0200

    Ubuntu_22.04 - packages: Comment out git-gui

--- Ubuntu_22.04/packages_install.sh
+++ Ubuntu_22.04/packages_install.sh
@@ -36,9 +36,9 @@ readonly APT_GUI_PACKAGES=(
     linux-tools-common linux-tools-generic linux-tools-"$(uname -r)"
 
     # ===== Editors, tools and IDEs =====
-    # TODO gitk breaks the install script (dependencies) - choose an other install method
+    # TODO gitk and git-gui break the install script (dependencies) - choose an other install method
     # it is just a bash script, hence it might make sense to install from source
-    meld kdevelop coqide qgit git-gui cmake-gui # gitk
+    meld kdevelop coqide qgit cmake-gui # gitk git-gui
 
     # ===== MESSAGING, PRODUCTIVITY, GRAPHICS, SOUND AND BOOKS =====
     gimp krita inkscape okular evince vlc audacity xdotool
