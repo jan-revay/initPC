3321943b8a18a93e46eccbf2a2b8cd606fb1d3da
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-08-17 11:21:02 +0200

    Ubuntu_22.04 - run_all.sh: `apt` -> `apt-get`

--- Ubuntu_22.04/run_all.sh
+++ Ubuntu_22.04/run_all.sh
@@ -13,8 +13,7 @@ gnome_present yes
 export NEEDRESTART_MODE=a
 export NEEDRESTART_SUSPEND=1
 
-sudo apt update
-sudo apt install chrome-gnome-shell
+sudo apt-get update
 # TODO move elsewhere...
 mkdir -p ~/tmp
 pushd ~/tmp
@@ -22,9 +21,10 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > linux_signi
 sudo install -D -o root -g root -m 644 linux_signing_key.pub /etc/apt/keyrings/linux_signing_key.pub
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/linux_signing_key.pub] http://dl.google.com/linux/chrome/deb/ stable main" \
     > /etc/apt/sources.list.d/google-chrome.list'
-sudo apt update
-sudo apt install google-chrome-stable
+sudo apt-get update
+sudo apt-get install google-chrome-stable
 popd
+sudo apt-get install chrome-gnome-shell
 
 # Run stuff that requires user input first (if not turned off by `--noninteractive`)
 . interactive_part.sh
