d5a77d07478dc659664eb2f5c27f6ac1bafa5430
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-08-17 11:22:53 +0200

    Ubuntu_22.04 - stow: Backup preexisting `logid.cfg`

--- Ubuntu_22.04/configs_install.sh
+++ Ubuntu_22.04/configs_install.sh
@@ -88,5 +88,11 @@ sudo update-alternatives --set editor /usr/bin/nvim
 
 pushd RootDotfiles/
 sudo stow -vvv --target=/ disable_highres_scroll/
-sudo stow -vvv --target=/ MX3_MASTER_LOGID_CONFIG/
+
+if stow -vvv --no --target=/ MX3_MASTER_LOGID_CONFIG/; then
+    sudo stow -vvv --target=/ MX3_MASTER_LOGID_CONFIG/
+else
+    sudo mv /etc/logid.cfg /etc/logid.cfg-initPCBackup"$(date '+%Y%m%d_%H%M%S')"
+    sudo stow -vvv --target=/ MX3_MASTER_LOGID_CONFIG/
+fi
 popd
