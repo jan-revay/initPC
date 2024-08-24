dcd4f72f4f44c9fb2113669ef82779b7b741d00d
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-08-24 21:10:20 +0200

    Android packages: Add `7zip`

--- Android_13/packages_install.sh
+++ Android_13/packages_install.sh
@@ -20,9 +20,9 @@ APT_PACKAGES+=(python python-numpy) # python
 APT_PACKAGES+=(nodejs ruby)         # JavaScript & Ruby
 APT_PACKAGES+=(neovim helix)        # code editors
 # utils - TODO organize
-APT_PACKAGES+=(cppcheck iwyu shfmt shellcheck)         # static analyzers
-APT_PACKAGES+=(valgrind doxygen)                       # other coding tools
-APT_PACKAGES+=(zsh fish tmux tree dust onefetch procs) # shells and commands
+APT_PACKAGES+=(cppcheck iwyu shfmt shellcheck)              # static analyzers
+APT_PACKAGES+=(valgrind doxygen)                            # other coding tools
+APT_PACKAGES+=(zsh fish tmux tree dust onefetch procs 7zip) # shells and commands
 APT_PACKAGES+=(tokei fd gh git htop bat exa jq man neofetch openssh ripgrep fzf)
 APT_PACKAGES+=(imagemagick)
 APT_PACKAGES+=(proot-distro) # ubuntu/debian emulation
