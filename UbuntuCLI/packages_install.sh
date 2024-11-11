101f84bbec0a2971551c681f01f54a3f83a1037d
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-11-11 16:49:26 +0100

    UbuntuCLI - packages: Add more CLI utils
    (zip file rsync openssh-client)

--- UbuntuCLI/packages_install.sh
+++ UbuntuCLI/packages_install.sh
@@ -26,7 +26,7 @@ LATEST_GCC_VER_IN_APT=$(apt-cache search --names-only '^gcc-[0-9][0-9]$' \
 # TODO add rclone
 APT_PACKAGES=(
     # GCC and build tools
-    build-essential gdb rr ccache ninja-build cmake cmake-gui
+    build-essential make gdb rr ccache ninja-build cmake cmake-gui
     gcc-"${LATEST_GCC_VER_IN_APT}" g++-"${LATEST_GCC_VER_IN_APT}"
 
     # " linux-tools-$(uname -r)" # TODO broken
@@ -70,7 +70,7 @@ APT_PACKAGES=(
     # TODO add more stress testing utilities
 
     # utilities
-    7zip
+    7zip zip file rsync openssh-client
 )
 
 # exa was replaced by eza on later versions of Ubuntu
