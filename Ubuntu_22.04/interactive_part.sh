1766c74b7ea4018d8a7b54d4e0107979a1acd03a
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-12-24 21:57:04 +0100

    Update interactive_part.sh

--- Ubuntu_22.04/interactive_part.sh
+++ Ubuntu_22.04/interactive_part.sh
@@ -12,13 +12,18 @@
 if [ "$1" == "--noninteractive" ]; then
     readonly NONINTERACTIVE='true'
     echo "Skipping interactive commands as $1 parameter was provided"
+    # TODO try configuring postfix noninteractively
+    # TODO format the code nicer...
+    sudo debconf-set-selections <<EOF
+    postfix postfix/main_mailer_type select No configuration
+EOF
+    sudo apt-get install -y postfix
 elif [ "$1" == "" ]; then
     # GNOME extensions
     # TODO configure postfix in advance from command line
     # TODO what is this app and why does it automatically install itself on Ubuntu
     # server/desktop, but not in wsl? It needs user interaction hence adding it here.
     sudo apt-get install -y postfix ttf-mscorefonts-installer
-
 fi
 
 . ../CommonInitScripts/gnome_install_extensions.sh
