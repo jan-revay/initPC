06951e3fe1691fa6198707754d025803fa0d05a1
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2024-12-25 00:40:22 +0100

    packages_install.sh - Add a TODO

--- UbuntuCLI/packages_install.sh
+++ UbuntuCLI/packages_install.sh
@@ -21,6 +21,17 @@ touch ~/.bashrc # some install scripts want to append stuff here
 # and then freeze the installation because postfix install script shows
 # an interactive menu.
 # TODO - investigate the postfix crash on ubuntu2VM
+# Dec 25 00:34:16 uVM systemd[1]: Created slice system-postfix.slice - Slice /system/postfix.
+# Dec 25 00:34:20 uVM systemd[1]: Starting postfix@-.service - Postfix Mail Transport Agent (instance -)...
+# Dec 25 00:34:20 uVM configure-instance.sh[2512]: postconf: fatal: open /etc/postfix/main.cf: No such file or directory
+# Dec 25 00:34:21 uVM configure-instance.sh[2850]: postconf: fatal: open /etc/postfix/main.cf: No such file or directory
+# Dec 25 00:34:22 uVM systemd[1]: postfix@-.service: Control process exited, code=exited, status=1/FAILURE
+# Dec 25 00:34:22 uVM systemd[1]: postfix@-.service: Failed with result 'exit-code'.
+# Dec 25 00:34:22 uVM systemd[1]: Failed to start postfix@-.service - Postfix Mail Transport Agent (instance -).
+# Dec 25 00:34:22 uVM systemd[1]: postfix.service - Postfix Mail Transport Agent was skipped because of an unmet condition check (ConditionPathExists=/etc/postfix/main.cf).
+# $ cat /etc/postfix/main.cf
+# cat: /etc/postfix/main.cf: No such file or directory
+# TODO -- maybe create just a minimal local config...
 sudo debconf-set-selections <<- EOF
     postfix postfix/main_mailer_type select No configuration
 EOF
