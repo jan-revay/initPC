a598632ad1629b833e17f63ca0d7966b75f1469b
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-03-15 11:09:44 +0100

    Overclocking - monitor.sh: Commit only once

--- Overclocking/Linux/monitor.sh
+++ Overclocking/Linux/monitor.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 
 if grep -i error ./output.log; then
+    if ls error.log; then
+        exit 0
+    fi
     grep -i error ./output.log | tee error.log
     git add error.log
     git commit -m "Overclocking - automatic error report"
