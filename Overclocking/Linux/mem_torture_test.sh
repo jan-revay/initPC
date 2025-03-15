cb0b0ab8dff4cd1bc80198418715e878dfeca64a
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-03-15 21:02:22 +0100

    Overclocking - Linux: Add more phoronix tests

--- Overclocking/Linux/mem_torture_test.sh
+++ Overclocking/Linux/mem_torture_test.sh
@@ -53,4 +53,13 @@ popd || exit
 ./burnintest/bit_cmd_line_x64
 
 phoronix-test-suite batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
-    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream
+    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
+    pts/hpcc-1.2.8 pts/hpl-1.0.0 pts/nero2d-1.1.0 pts/nwchem-1.2.0 pts/open-porous-media-1.3.1 \
+    pts/opm-git-1.3.51 pts/qe-1.3.1 pts/qmcpack-1.8.0 pts/wrf-1.0.1
+
+phoronix-test-suite stress-batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
+    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
+    pts/hpcc-1.2.8 pts/hpl-1.0.0 pts/nero2d-1.1.0 pts/nwchem-1.2.0 pts/open-porous-media-1.3.1 \
+    pts/opm-git-1.3.51 pts/qe-1.3.1 pts/qmcpack-1.8.0 pts/wrf-1.0.1
+
+phoronix-test-suite stress-batch-run pts/lapack pts/blas pts/compiler
