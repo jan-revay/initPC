43d15afed36587bcf064682d3aaa7c3288df5073
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-03-15 20:13:35 +0100

    Overclocking - Linux: Bugfix - install phoronix-test-suite manually

--- UbuntuCLI/optional_packages_install.sh
+++ UbuntuCLI/optional_packages_install.sh
@@ -12,7 +12,11 @@
 
 OPT_APT_PACKAGES=(
     # stress testing and benchmarking tools
-    stressapptest phoronix-test-suite stress-ng inxi
+    stressapptest stress-ng inxi
 )
 
 time sudo apt-get install -y "${OPT_APT_PACKAGES[@]}"
+
+wget -P /tmp/ https://github.com/phoronix-test-suite/phoronix-test-suite/releases/download/v10.8.4/phoronix-test-suite_10.8.4_all.deb
+sudo dpkg -i /tmp/phoronix-test-suite*
+rm /tmp/phoronix-test-suite*
