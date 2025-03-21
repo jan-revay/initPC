0696b935246e4a20ab93c3451ac247b530a29373
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-03-21 12:06:46 +0100

    Overclocking - Linux: monitor.sh - add regex

--- Overclocking/Linux/monitor.sh
+++ Overclocking/Linux/monitor.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -x
 
-if grep -i error ./output.log; then
+NEEDLE='error\|exception\|fail\|fatal\|segfault\|corrupt\|abort\|warn\|unable\|bad\|wrong\|incorrect'
+
+if grep -i "${NEEDLE}" bench.log bench_mini.log \
+    output.log ./p95*/results.txt; then
+
     if ls error.log; then
         exit 0
     fi
-    grep -i error ./output.log | tee error.log
+
+    grep -i "${NEEDLE}" bench.log bench_mini.log output.log | tee error.log
     git add error.log
     git commit -m "Overclocking - automatic error report"
     git push
