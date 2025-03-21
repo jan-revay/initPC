c7084d07363fece87b595d3f6657b6984727b182
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-03-21 12:08:42 +0100

    Overclocking - Linux: mem_torture_test.sh - move stuff around

--- Overclocking/Linux/mem_torture_test.sh
+++ Overclocking/Linux/mem_torture_test.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -x
 
 # save a copy of the output to a file
-exec &> >(tee output.log)
+exec &>> >(tee --append output.log)
 
-inxi -Fxxxz
-phoronix-test-suite batch-run pts/ramspeed pts/tinymembench
-memtester 10G 10
+# echo ============ TEST BEGIN ==============
+#
+# inxi -Fxxxz
+#
+# pushd p95* || exit
+# # Results are in ./results.txt file
+# timeout --preserve-status 28h ./mprime -t
+# popd || exit
+#
+# phoronix-test-suite batch-run pts/ramspeed pts/tinymembench
+# memtester 10G 10
 # phoronix-test-suite batch-install pts/ramspeed
 
 # phoronix-test-suite batch-setup
@@ -20,46 +28,47 @@ memtester 10G 10
 #    pts/m-queens-1.1.0 pts/glibc-bench-1.9.0 pts/encode-flac-1.9.0 pts/compress-7zip-1.11.0 pts/byte-1.3.0 \
 #    pts/blender-4.3.0
 
-flatpak run com.geekbench.Geekbench6
+# flatpak run com.geekbench.Geekbench6
+#
+# # stress-ng is also avaliable in phoronix test suite
+# stress-ng --tz --metrics-brief --matrix 0 -t 2h
+# stress-ng --vm 96 --vm-bytes 95% --vm-method all --tz --metrics-brief --timeout 1h
+# stress-ng --memrate 0 --metrics-brief --timeout 30m
+# stress-ng --cache 0 --cache-fence --metrics-brief --timeout 30m
+# stress-ng --matrix 0 --vm 96 --vm-bytes 95% --cache 0 --cache-fence --tz --metrics-brief --timeout 1h
+#
+# stressapptest -W -M "$(free -m | awk '/Mem:/ {print int($2 * 0.95)}')" -s 65000 \
+#     || timeout 2s speaker-test -t sine -f 1000
 
-pushd p95* || exit
-# Results are in ./results.txt file
-timeout --preserve-status 28h ./mprime -t \
-    || timeout 2s speaker-test -t sine -f 1000
+# pts/nero2d-1.1.0 was removed because it segfaults
 
-popd || exit
+timeout 30 yes "6000mt_pboe2_90_R4" | phoronix-test-suite batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
+    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
+    pts/hpcc-1.2.8 pts/hpl-1.0.0 pts/nwchem-1.2.0 \
+    pts/opm-git-1.3.51 pts/qe-1.3.1 pts/qmcpack-1.8.0 pts/wrf-1.0.1
 
-if grep -i error output.log; then
-    timeout 2s speaker-test -t sine -f 1000
-fi
+timeout 30 yes "6000mt_pboe2_90_R4" | phoronix-test-suite stress-batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
+    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
+    pts/hpcc-1.2.8 pts/hpl-1.0.0 pts/nwchem-1.2.0 \
+    pts/opm-git-1.3.51 pts/qe-1.3.1 pts/qmcpack-1.8.0 pts/wrf-1.0.1
 
-# stress-ng is also avaliable in phoronix test suite
-stress-ng --tz --metrics-brief --matrix 0 -t 2h
-stress-ng --vm 96 --vm-bytes 95% --vm-method all --tz --metrics-brief --timeout 1h
-stress-ng --memrate 0 --metrics-brief --timeout 30m
-stress-ng --cache 0 --cache-fence --metrics-brief --timeout 30m
-stress-ng --matrix 0 --vm 96 --vm-bytes 95% --cache 0 --cache-fence --tz --metrics-brief --timeout 1h
-
-stressapptest -W -M "$(free -m | awk '/Mem:/ {print int($2 * 0.95)}')" -s 65000 \
-    || timeout 2s speaker-test -t sine -f 1000
+timeout 30 yes "6000mt_pboe2_90_R4" | phoronix-test-suite stress-batch-run pts/lapack pts/blas pts/compiler system/scientific
 
 pushd y-cruncher* || exit
-for i in {1..600}; do
+for i in {1..400}; do
     echo "===== Iteration $i ====="
+    # TODO - run stress test ./y-cruncher stress instead of
+    # bench. TODO - have a look at ./y-cruncher*/Command lines.txt
     ./y-cruncher bench 16G || timeout 2s speaker-test -t sine -f 1000
 done
 popd || exit
 
 ./burnintest/bit_cmd_line_x64
 
-phoronix-test-suite batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
-    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
-    pts/hpcc-1.2.8 pts/hpl-1.0.0 pts/nero2d-1.1.0 pts/nwchem-1.2.0 pts/open-porous-media-1.3.1 \
-    pts/opm-git-1.3.51 pts/qe-1.3.1 pts/qmcpack-1.8.0 pts/wrf-1.0.1
+# for i in {1..600}; do
+#     echo "===== Iteration $i ====="
+#     timeout --preserve-status 1h ./p95*/mprime -t
+#     ./y-cruncher*/y-cruncher bench 16G || timeout 2s speaker-test -t sine -f 1000
+# done
 
-phoronix-test-suite stress-batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
-    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
-    pts/hpcc-1.2.8 pts/hpl-1.0.0 pts/nero2d-1.1.0 pts/nwchem-1.2.0 pts/open-porous-media-1.3.1 \
-    pts/opm-git-1.3.51 pts/qe-1.3.1 pts/qmcpack-1.8.0 pts/wrf-1.0.1
-
-phoronix-test-suite stress-batch-run pts/lapack pts/blas pts/compiler system/scientific
+echo ========= TEST END ========
