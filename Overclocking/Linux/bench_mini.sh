03eec54092b260a13cc3a81810bb2e85eee9c593
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-03-24 15:30:09 +0100

    Overclocking - Linux - bench_mini.sh: Add benchmarks

--- Overclocking/Linux/bench_mini.sh
+++ Overclocking/Linux/bench_mini.sh
@@ -3,14 +3,18 @@
 # save a copy of the output to a file
 exec &>> >(tee bench_mini.log)
 
-phoronix-test-suite batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
-    pts/ramspeed pts/tinymembench
+# phoronix-test-suite batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
+# pts/ramspeed pts/tinymembench
 
 flatpak run com.geekbench.Geekbench6
+
+phoronix-test-suite batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
+    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
+    pts/ramspeed pts/tinymembench
 
 pushd y-cruncher* || exit
 for i in {1..3}; do
     echo "===== Iteration $i ====="
-    ./y-cruncher bench 16G || timeout 2s speaker-test -t sine -f 1000
+    ./y-cruncher bench 16G
 done
 popd || exit
