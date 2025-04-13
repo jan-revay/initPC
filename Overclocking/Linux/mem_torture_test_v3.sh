965869c1c7bbe1c851d5451f3a1d49e489f3dd44
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-04-13 20:51:11 +0200

    mem_torture_test_v3.sh: Add current

--- Overclocking/Linux/mem_torture_test_v3.sh
+++ Overclocking/Linux/mem_torture_test_v3.sh
@@ -10,16 +10,19 @@ sudo apt-get -y install memtester stress-ng
 # echo ============ TEST BEGIN ==============
 #
 inxi -Fxxxz
+pushd ~/x/linux || exit
+sleep 50 && make clean > /dev/null && time make -j"$(nproc)" > /dev/null && make clean > /dev/null && sleep 20 && time make -j"$(nproc)" > /dev/null && sleep 60 && flatpak run com.geekbench.Geekbench6
+popd || exit
 
 # TODO add date so that every time is the ID unique
 # store ID in a separate variable
 # store params in a separate variables and assamble them here
-TEST_ID="APU26200MT_COCCX1-20_COCCX2-22_S1_O25_RUN1_ID7sj27r"
+TEST_ID="APU26200MT_CO20_RUN1_ID7sj27r"
 
-TOTAL_LOOP_TIME=120 PTS_CONCURRENT_TEST_RUNS=4 phoronix-test-suite stress-batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
-    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
-    pts/hpcc-1.2.8 pts/hpl-1.0.0 pts/nwchem-1.2.0 \
-    pts/opm-git-1.3.51 pts/qe-1.3.1 pts/qmcpack-1.8.0 pts/wrf-1.0.1
+# TOTAL_LOOP_TIME=120 phoronix-test-suite stress-batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
+#     pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
+#     pts/hpcc-1.2.8 pts/hpl-1.0.0 pts/nwchem-1.2.0 \
+#     pts/opm-git-1.3.51 pts/qe-1.3.1 pts/qmcpack-1.8.0 pts/wrf-1.0.1
 
 sleep 120 && flatpak run com.geekbench.Geekbench6
 sleep 180
@@ -35,9 +38,11 @@ stress-ng --matrix 0 --vm 96 --vm-bytes 95% --cache 0 --cache-fence --tz --metri
 
 stressapptest -W -M "$(free -m | awk '/Mem:/ {print int($2 * 0.95)}')" -s 65000
 
+sleep 120 && flatpak run com.geekbench.Geekbench6
+
 for i in {1..118}; do
     echo "===== Iteration $i ====="
-    timeout --preserve-status 20m ./p95*/mprime -t
+    timeout --preserve-status 40m ./p95*/mprime -t
     # shellcheck disable=SC2211
     ./y-cruncher*/y-cruncher bench 16G
     # shellcheck disable=SC2211
@@ -45,10 +50,9 @@ for i in {1..118}; do
 done
 
 # frome bench_mini.sh
-# timeout 3 yes "${TEST_ID}" | phoronix-test-suite batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
-#     pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
-#     pts/ramspeed pts/tinymembench
-#
+timeout 3 yes "${TEST_ID}_bench" | phoronix-test-suite batch-run system/compress-7zip-1.0.0 pts/build-linux-kernel-1.16.0 pts/build-php-1.7.0 \
+    pts/blender-4.3.0 pts/compress-7zip-1.11.0 pts/encode-flac-1.9.0 pts/mbw pts/stream \
+    pts/ramspeed pts/tinymembench
 
 # pushd p95* || exit
 # # Results are in ./results.txt file
@@ -87,7 +91,7 @@ done
 #     pts/hpcc-1.2.8 pts/hpl-1.0.0 pts/nwchem-1.2.0 \
 #     pts/opm-git-1.3.51 pts/qe-1.3.1 pts/qmcpack-1.8.0 pts/wrf-1.0.1
 #
-timeout 3 yes "${TEST_ID}" | phoronix-test-suite stress-batch-run pts/lapack pts/blas pts/compiler system/scientific
+timeout 3 yes "${TEST_ID}_test_suites" | phoronix-test-suite stress-batch-run pts/lapack pts/blas pts/compiler system/scientific
 
 # pushd y-cruncher* || exit
 # for i in {1..400}; do
