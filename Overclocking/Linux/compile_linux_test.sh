f9a4a8afd4785e214b9d9d1a2a0fc400f51976a3
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-06-12 18:03:43 +0200

    Overclocking - build linux kernel: Install dpkg dependencies

--- Overclocking/Linux/compile_linux_test.sh
+++ Overclocking/Linux/compile_linux_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+sudo apt-get install -y build-essential libncurses-dev bison flex libssl-dev libelf-dev bc
 git clone https://github.com/torvalds/linux.git
 cd linux/ || exit 1
 git checkout cd2e103d57e5615f9bb027d772f93b9efd567224 # 1.6. 2025
@@ -18,4 +19,3 @@ sleep 30
 time make -j"$(nproc)" > /dev/null
 make clean > /dev/null
 sleep 30
-
