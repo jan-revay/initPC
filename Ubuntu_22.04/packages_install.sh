c3201e897985cfebb1f73e58baebd9846949ae0f
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-06-12 12:44:02 +0200

    Ubuntu 25.04: Remove Google Chrome - bad repo

--- Ubuntu_22.04/packages_install.sh
+++ Ubuntu_22.04/packages_install.sh
@@ -36,13 +36,13 @@ pushd ~/tmp
 
 # Install Google Chrome and Chrome extension for managing GNOME extensions
 sudo apt-get install -y wget gpg
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > linux_signing_key.pub
-sudo install -D -o root -g root -m 644 linux_signing_key.pub /etc/apt/keyrings/linux_signing_key.pub
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/linux_signing_key.pub] http://dl.google.com/linux/chrome/deb/ stable main" \
-    > /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get -y update
-sudo apt-get install -y google-chrome-stable chrome-gnome-shell
-rm linux_signing_key.pub
+# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub > linux_signing_key.pub
+# sudo install -D -o root -g root -m 644 linux_signing_key.pub /etc/apt/keyrings/linux_signing_key.pub
+# sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/linux_signing_key.pub] http://dl.google.com/linux/chrome/deb/ stable main" \
+#    > /etc/apt/sources.list.d/google-chrome.list'
+# sudo apt-get -y update
+# sudo apt-get install -y google-chrome-stable chrome-gnome-shell
+# rm linux_signing_key.pub
 
 # Install VSC (Snap package is problematic for C++ because of issues with libraries)
 # echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
