52204a50657162b4899a4ca830c1880624fb8968
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-06-12 12:39:42 +0200

    Ubuntu 25.04 - quick fix: remove incompatible packages

--- UbuntuCLI/optional_packages_install.sh
+++ UbuntuCLI/optional_packages_install.sh
@@ -11,22 +11,22 @@
 . ../prelude.sh
 
 OPT_APT_PACKAGES=(
+    stressapptest stress-ng inxi
     # stress testing and benchmarking tools
     # php is required by phoronix test suite
     # todo fix php on ubuntu 22.04
-    stressapptest stress-ng inxi
 
     # phoronix-test-suite dependencies
-    php8.3 php-xml php8.3-xml
-    php-gd php-bz2 php-sqlite3 php-curl
-    libpopt-dev # TODO mpicc
+    #    php8.3 php-xml php8.3-xml
+    #    php-gd php-bz2 php-sqlite3 php-curl
+    #    libpopt-dev # TODO mpicc
 )
 
 time sudo apt-get install -y "${OPT_APT_PACKAGES[@]}"
 
-wget -P /tmp/ https://github.com/phoronix-test-suite/phoronix-test-suite/releases/download/v10.8.4/phoronix-test-suite_10.8.4_all.deb
-sudo dpkg -i /tmp/phoronix-test-suite_10.8.4_all.deb
+# wget -P /tmp/ https://github.com/phoronix-test-suite/phoronix-test-suite/releases/download/v10.8.4/phoronix-test-suite_10.8.4_all.deb
+# sudo dpkg -i /tmp/phoronix-test-suite_10.8.4_all.deb
 
-rm /tmp/phoronix-test-suite_10.8.4_all.deb
+# rm /tmp/phoronix-test-suite_10.8.4_all.deb
 
-phoronix-test-suite batch-install pts/disk
+# phoronix-test-suite batch-install pts/disk
