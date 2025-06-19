689b78ce670f15e1cbbfbe92218d68e64fbfe87e
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-06-19 14:48:41 +0200

    Ubuntu - packages: Skip broken VSCode installation

--- Ubuntu_22.04/packages_install.sh
+++ Ubuntu_22.04/packages_install.sh
@@ -44,17 +44,18 @@ sudo apt-get install -y wget gpg
 # sudo apt-get install -y google-chrome-stable chrome-gnome-shell
 # rm linux_signing_key.pub
 
+# TODO fix VSC and Google chrome installation
 # Install VSC (Snap package is problematic for C++ because of issues with libraries)
 # echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" \
-    | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
+# wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+# sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+# echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" \
+# | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+# rm -f packages.microsoft.gpg
 
 sudo apt-get install -y apt-transport-https
 sudo apt-get -y update
-sudo apt-get install -y code # or code-insiders
+# sudo apt-get install -y code # or code-insiders
 popd
 
 echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula boolean true" \
