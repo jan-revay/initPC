ce050235aaf0dbf617b96255b8c8c58937836d2d
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-11-03 16:13:03 +0100

    GNOME extensions dash-to-dock: Turn off <Super>+1-9 keybinding

--- Ubuntu_22.04/configs_install.sh
+++ Ubuntu_22.04/configs_install.sh
@@ -58,7 +58,8 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'previews'
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'LEFT'
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height 'false'
+gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys 'false'
 gsettings set org.gnome.shell.extensions.dash-to-dock pressure-threshold '50.0'
 gsettings set org.gnome.shell.extensions.dash-to-dock preview-size-scale '0.25'
 gsettings set org.gnome.shell.extensions.dash-to-dock shift-click-action 'minimize'
