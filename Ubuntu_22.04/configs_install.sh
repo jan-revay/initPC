fe6c8ce216037379e56c7c2fe2d5619a937d142f
TheProductiveDeveloper <65253638+jan-revay@users.noreply.github.com>
2025-11-12 01:32:42 +0100

    Enhance configs_install.sh with TODO comments
    Added comments and TODOs for future enhancements regarding GNOME terminal settings and gsettings usage.

--- Ubuntu_22.04/configs_install.sh
+++ Ubuntu_22.04/configs_install.sh
@@ -12,6 +12,26 @@
 # TODO consider using dconf dump & dconf load instead of calling gsettings for every key
 # see: https://askubuntu.com/questions/984205/how-to-save-gnome-settings-in-a-file
 
+
+# TODO - use sth like this:
+# apply_gsettings() {
+ # local schema="$1"
+ # while IFS= read -r line; do
+ #     [[ -z "$line" || "$line" =~ ^# ]] && continue
+ #     gsettings set "$schema" "$line"
+ # done
+ # }
+ #
+ # apply_gsettings "test" <<EOF
+ # ano
+ # nie #blabla
+ # mozno
+ #
+ # # lala
+  # sjsjdj
+ #
+ # EOF
+
 # TODO pohrat sa s tymi nastaveniami, ci vlastne su vsetky potrebne a robia to co chcem
 
 . ../prelude.sh
