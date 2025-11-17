73586a73b75866a570a31d54835d74ce4535630d
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-11-17 18:11:50 +0100

    extensions_gsettings.sh: Sort for_each blocks and keys

--- Ubuntu_22.04/extensions_gsettings.sh
+++ Ubuntu_22.04/extensions_gsettings.sh
@@ -9,6 +9,45 @@ glib-compile-schemas "${SCHDIR}"
 # See: https://docs.gtk.org/gio/overview.html#running-gio-applications
 export GSETTINGS_SCHEMA_DIR="${SCHDIR}"
 
+# NOTE:
+# 1. All for_each blocks are sorted by their respective gsettings path
+# 2. All keys in a specific for_each block are sorted alphabetically
+
+# clipboard-history@alexsaveau.dev
+for_each "gsettings set org.gnome.shell.extensions.clipboard-history" << 'BASH'
+    cache-size "1000"
+    display-mode "1"
+    history-size "10000"
+    next-entry "['<Alt>v']"
+    prev-entry "['<Shift><Alt>v']"
+    toggle-menu "['<Super>v']"
+    toggle-private-mode "[]"
+    topbar-preview-size "45"
+    window-width-percentage "100"
+BASH
+
+# ubuntu-dock@ubuntu.com
+for_each "gsettings set org.gnome.shell.extensions.dash-to-dock" << 'BASH'
+    always-center-icons "true"
+    animation-time "0.01"
+    click-action "previews"
+    dash-max-icon-size "64"
+    default-windows-preview-to-open "true"
+    dock-fixed "false"
+    dock-position "LEFT"
+    extend-height "false"
+    hot-keys "false"
+    pressure-threshold "50.0"
+    preview-size-scale "0.25"
+    shift-click-action "minimize"
+    shift-middle-click-action "launch"
+    shortcut-timeout "4"
+    show-mounts "false"
+    show-show-apps-button "false"
+    show-trash "false"
+    show-windows-preview "true"
+BASH
+
 # focus-changer@heartmire
 for_each "gsettings set org.gnome.shell.extensions.focus-changer" << 'BASH'
     focus-down "['<Shift><Control><Alt><Super>Down']"
@@ -35,69 +74,25 @@ for_each "gsettings set org.gnome.shell.extensions.quake-terminal" << 'BASH'
     vertical-size "50"
 BASH
 
-# clipboard-history@alexsaveau.dev
-for_each "gsettings set org.gnome.shell.extensions.clipboard-history" << 'BASH'
-    cache-size "1000"
-    display-mode "1"
-    history-size "10000"
-    next-entry "['<Alt>v']"
-    prev-entry "['<Shift><Alt>v']"
-    toggle-menu "['<Super>v']"
-    toggle-private-mode "[]"
-    topbar-preview-size "45"
-    window-width-percentage "100"
+# system-monitor-next@paradoxxx.zero.gmail.com
+for_each "gsettings set org.gnome.shell.extensions.system-monitor-next-applet" << 'BASH'
+    cpu-system-color "#c01c28ff"
+    rotate-labels "true"
+    show-tooltip "true"
 BASH
-
-# TODO add the remaining settings
-gsettings set org.gnome.shell.extensions.vitals fixed-widths "false"
 
 # tiling-assistant@ubuntu.com
 for_each "gsettings set org.gnome.shell.extensions.tiling-assistant" << 'BASH'
-    enable-advanced-experimental-features "true"
-    window-gap "8"
-    focus-hint "3"
-    focus-hint-outline-style "1"
-    focus-hint-color "rgb(192,97,203)"
-    focus-hint-outline-size "6"
-    focus-hint-outline-border-radius "6"
-    enable-tiling-popup "false"
-    tiling-popup-all-workspace "false"
     dynamic-keybinding-behavior "0"
-BASH
-
-# ubuntu-dock@ubuntu.com
-for_each "gsettings set org.gnome.shell.extensions.dash-to-dock" << 'BASH'
-    always-center-icons "true"
-    animation-time "0.01"
-    click-action "previews"
-    dash-max-icon-size "64"
-    default-windows-preview-to-open "true"
-    dock-fixed "false"
-    dock-position "LEFT"
-    extend-height "false"
-    hot-keys "false"
-    pressure-threshold "50.0"
-    preview-size-scale "0.25"
-    shift-click-action "minimize"
-    shift-middle-click-action "launch"
-    shortcut-timeout "4"
-    show-mounts "false"
-    show-show-apps-button "false"
-    show-trash "false"
-    show-windows-preview "true"
-BASH
-
-# workspaces-by-open-apps@favo02.github.com
-for_each "gsettings set org.gnome.shell.extensions.workspaces-indicator-by-open-apps" << 'BASH'
-    indicator-show-background "true"
-    scroll-enable "false"
-BASH
-
-# system-monitor-next@paradoxxx.zero.gmail.com
-for_each "gsettings set org.gnome.shell.extensions.system-monitor-next-applet" << 'BASH'
-    show-tooltip "true"
-    rotate-labels "true"
-    cpu-system-color "#c01c28ff"
+    enable-advanced-experimental-features "true"
+    enable-tiling-popup "false"
+    focus-hint "3"
+    focus-hint-color "rgb(192,97,203)"
+    focus-hint-outline-border-radius "6"
+    focus-hint-outline-size "6"
+    focus-hint-outline-style "1"
+    tiling-popup-all-workspace "false"
+    window-gap "8"
 BASH
 
 # tilingshell@ferrarodomenico.com
@@ -111,5 +106,18 @@ for_each "gsettings set org.gnome.shell.extensions.tilingshell" << 'BASH'
     overridden-settings '{"org.gnome.mutter":{"edge-tiling":"false"}}'
 BASH
 
+# Vitals@CoreCoding.com
+for_each "gsettings set org.gnome.shell.extensions.vitals" << 'BASH'
+    gsettings set org.gnome.shell.extensions.vitals fixed-widths "false"
+BASH
+
 # window-title-is-back@fthx
-gsettings set org.gnome.shell.extensions.window-title-is-back fixed-width "false"
+for_each "gsettings set org.gnome.shell.extensions.window-title-is-back" << 'BASH'
+    gsettings set org.gnome.shell.extensions.window-title-is-back fixed-width "false"
+BASH
+
+# workspaces-by-open-apps@favo02.github.com
+for_each "gsettings set org.gnome.shell.extensions.workspaces-indicator-by-open-apps" << 'BASH'
+    indicator-show-background "true"
+    scroll-enable "false"
+BASH
