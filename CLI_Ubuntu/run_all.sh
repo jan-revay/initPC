7bd148c7640c52b1789b930081344ae9ef72f0bb
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-11-18 10:23:27 +0100

    buntu_specific_packages.sh: Delete

--- CLI_Ubuntu/run_all.sh
+++ CLI_Ubuntu/run_all.sh
@@ -7,9 +7,7 @@ distro_is ubuntu
 distro_version_ge 22
 gnome_present no
 
-. ubuntu_specific_packages.sh
 . packages_install.sh
-. ubuntu_specific_packages.sh
 . optional_packages_install.sh
 # stuff that requires user input can be turned off by `--noninteractive` CLI param
 . interactive_part.sh
--- CLI_Ubuntu/ubuntu_specific_packages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-# The script should not require any user input and should be idempotent.
-
-. ../prelude.sh
-
-# TODO - remove this file if it will not be used
