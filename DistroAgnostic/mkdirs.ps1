8c3e63ebae875291cb59ee331519e39c453baf61
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-11-18 14:48:37 +0100

    Move ../DistroAgnostic/mkdirs.ps1 to initPC_Windows

--- DistroAgnostic/mkdirs.ps1
+++
@@ -1,6 +0,0 @@
-# TODO - is the -Force parameter really needed here?
-
-mkdir -Force ~/books
-mkdir -Force ~/x # all my development repos go here
-mkdir -Force ~/x/tmp # stuff that can be deleted after restart
-
