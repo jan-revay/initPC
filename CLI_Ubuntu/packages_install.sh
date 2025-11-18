920cb0a2a32fa0adde3529fc0a614d58277d0226
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-11-18 10:31:38 +0100

    optional_packages_install.sh: Merge with packages_install.sh

--- CLI_Ubuntu/packages_install.sh
+++ CLI_Ubuntu/packages_install.sh
@@ -183,3 +183,36 @@ time sudo gem install mdl # markdown-lint
 # And optional remove the obsolete apt pipx!
 # sudo apt purge --autoremove pipx
 # https://github.com/pypa/pipx/issues/1481
+
+
+# ----------------
+
+# TODO add:
+#    - zig compiler and language server
+#    - rust compiler
+#    - ICC and AOCC (Intel and AMD C++ compilers)
+# add foliate: sudo apt install foliate
+#    - powershell https://learn.microsoft.com/en-us/powershell/scripting/install/install-ubuntu?view=powershell-7.3
+
+. ../prelude.sh
+
+OPT_APT_PACKAGES=(
+    stressapptest stress-ng inxi
+    # stress testing and benchmarking tools
+    # php is required by phoronix test suite
+    # todo fix php on ubuntu 22.04
+
+    # phoronix-test-suite dependencies
+    #    php8.3 php-xml php8.3-xml
+    #    php-gd php-bz2 php-sqlite3 php-curl
+    #    libpopt-dev # TODO mpicc
+)
+
+time sudo apt-get install -y "${OPT_APT_PACKAGES[@]}"
+
+# wget -P /tmp/ https://github.com/phoronix-test-suite/phoronix-test-suite/releases/download/v10.8.4/phoronix-test-suite_10.8.4_all.deb
+# sudo dpkg -i /tmp/phoronix-test-suite_10.8.4_all.deb
+
+# rm /tmp/phoronix-test-suite_10.8.4_all.deb
+
+# phoronix-test-suite batch-install pts/disk
--- CLI_Ubuntu/run_all.sh
+++ CLI_Ubuntu/run_all.sh
@@ -8,7 +8,6 @@ distro_version_ge 22
 gnome_present no
 
 . packages_install.sh
-. optional_packages_install.sh
 # stuff that requires user input can be turned off by `--noninteractive` CLI param
 . interactive_part.sh
 . configs_install.sh
