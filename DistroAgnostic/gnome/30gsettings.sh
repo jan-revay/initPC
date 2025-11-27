ef3dece805dc55e4767c762bb3ed32577a63f6b0
TheProductiveDeveloper <65253638+jan-revay@users.noreply.github.com>
2025-11-27 13:35:13 +0100

    gsettings: Add a TODO

--- DistroAgnostic/gnome/30gsettings.sh
+++ DistroAgnostic/gnome/30gsettings.sh
@@ -20,6 +20,7 @@ gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
 
 # Ptyxis and accent-color are not present on Ubuntu 24.04 - check and skip
+# TODO remove the ifdef after Ubuntu 26.04 is avaliable is GitHub Actions runner
 if gsettings get org.gnome.Ptyxis scrollbar-policy && gsettings get org.gnome.desktop.interface accent-color; then
 
     for_each "gsettings set org.gnome.Ptyxis " << 'BASH'
