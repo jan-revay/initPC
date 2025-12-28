a09b2a15740e38043ce515b228a31deeeff94a42
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-12-28 18:27:24 +0100

    run_all.sh: Add a TODO

--- CLI_Ubuntu/run_all.sh
+++ CLI_Ubuntu/run_all.sh
@@ -2,6 +2,7 @@
 . ../prelude.sh
 # This script should be idempotent.
 
+# TODO consider using run-parts utility for running the scripts
 if distro_is ubuntu && distro_version_ge 24; then
     # stuff that requires user input can be turned off by `--noninteractive` CLI param
     for script in [0-9][0-9]*.sh; do
