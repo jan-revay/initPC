521bfc31c4d80a871c5b33cf9dbe09b514f8b9fe
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2025-12-29 10:53:23 +0100

    RootDotfiles: Add spin_hdd_off

--- DistroAgnostic/50root_dotfiles.sh
+++ DistroAgnostic/50root_dotfiles.sh
@@ -3,15 +3,23 @@
 
 pushd RootDotfiles/
 
-sudo chown root:root ./daily_schutdown_schedule/etc/cron.d/daily_shutdown_schedule
-sudo chmod 644 ./daily_schutdown_schedule/etc/cron.d/daily_shutdown_schedule
+CROND_FILES=(./daily_schutdown_schedule/etc/cron.d/daily_shutdown_schedule
+    ./spin_hdd_off/etc/cron.d/spin_hdd_off
+)
+
+sudo chown root:root "${CROND_FILES[@]}"
+sudo chmod 644 "${CROND_FILES[@]}"
 
 if ! stow -vvv --no --target=/ MX3_MASTER_LOGID_CONFIG/; then
     sudo mv /etc/logid.cfg /etc/logid.cfg-initPCBackup"$(date '+%Y%m%d_%H%M%S')"
 fi
 
 # TODO - modify dirs_to_stow based on platform (e.g. VM)
-dirs_to_stow=(disable_highres_scroll daily_schutdown_schedule MX3_MASTER_LOGID_CONFIG)
+dirs_to_stow=(disable_highres_scroll
+    MX3_MASTER_LOGID_CONFIG
+    spin_hdd_off
+    daily_schutdown_schedule
+)
 
 # Stow each directory
 for dir in "${dirs_to_stow[@]}"; do
