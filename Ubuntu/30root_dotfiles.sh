171483a05db34cdc3c4f3758abc0634a7b534c82
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-01-11 13:52:49 +0100

    RootDotfiles: Clean before installing & complete refactor

--- Ubuntu/30root_dotfiles.sh
+++ Ubuntu/30root_dotfiles.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 . ../prelude.sh
-# TODO add an option to clean the stowed files
+
+clean(){
+    local readonly TO_CLEAN=(
+        /etc/cron.d/daily_shutdown_schedule
+        /etc/cron.d/spin_hdd_off
+        /etc/libinput/local-overrides.quirks
+    )
+    # Delete links to files created by this script
+    sudo rm -f "${TO_CLEAN}"
+    # Backup old logid.cfg
+    if ! stow -vvv --no --target=/ MX3_MASTER_LOGID_CONFIG/; then
+        sudo mv /etc/logid.cfg /etc/logid.cfg-initPCBackup"$(date '+%Y%m%d_%H%M%S')"
+    fi
+}
+
+permissions() {
+    local readonly CROND_FILES=(
+        ./daily_schutdown_schedule/etc/cron.d/daily_shutdown_schedule
+        ./spin_hdd_off/etc/cron.d/spin_hdd_off
+    )
+
+    # TODO anytime git touches these files, permissions will be messed up?
+    # Is it ok? Consider copying directly.
+    sudo chown root:root "${CROND_FILES[@]}"
+    sudo chmod 644 "${CROND_FILES[@]}"
+}
+
+stow_all() {
+    # TODO - modify dirs_to_stow based on platform (e.g. VM)
+    local readonly DIRS_TO_STOW=(
+        MX3_MASTER_LOGID_CONFIG
+        daily_schutdown_schedule
+        disable_highres_scroll
+        spin_hdd_off
+    )
+
+    sudo stow -vvv --target=/ ${DIRS_TO_STOW[@]}
+}
+
+# We create libinput dir explicitly instead of just linking to it with stow,
+# so that we can clean just the file we stowed there (the dir might be used
+# by other utils as well), we don't want stow to own the whole directory.
+sudo mkdir -p /etc/libinput
+
 pushd RootDotfiles/
-
-CROND_FILES=(./daily_schutdown_schedule/etc/cron.d/daily_shutdown_schedule
-    ./spin_hdd_off/etc/cron.d/spin_hdd_off
-)
-
-# TODO anytime git touches these files, permissions will be messed up.
-# Is it ok? Consider copying directly.
-#
-sudo chown root:root "${CROND_FILES[@]}"
-sudo chmod 644 "${CROND_FILES[@]}"
-
-if ! stow -vvv --no --target=/ MX3_MASTER_LOGID_CONFIG/; then
-    sudo mv /etc/logid.cfg /etc/logid.cfg-initPCBackup"$(date '+%Y%m%d_%H%M%S')"
-fi
-
-# TODO - modify dirs_to_stow based on platform (e.g. VM)
-dirs_to_stow=(disable_highres_scroll
-    MX3_MASTER_LOGID_CONFIG
-    spin_hdd_off
-    daily_schutdown_schedule
-)
-
-# Stow each directory
-for dir in "${dirs_to_stow[@]}"; do
-    sudo stow -vvv --target=/ "$dir"/
-done
-
+clean
+permissions
+stow_all
 popd
