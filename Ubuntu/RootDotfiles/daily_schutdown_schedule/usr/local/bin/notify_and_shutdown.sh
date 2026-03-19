69bae42fdd2fc0ee136d5d12e789a1f149695685
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-03-19 20:02:33 +0100

    horarium: Simplify - remove broken notify-send script

--- Ubuntu/RootDotfiles/daily_schutdown_schedule/usr/local/bin/notify_and_shutdown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# TODO fix the notify-send https://www.perplexity.ai/search/notify-send-does-not-work-when-iCt5Y1qiSW.ZRYifyqzTug
-
-/usr/bin/notify-send --transient --urgency critical "Shutting down in 1 min" "$1"
-/sbin/shutdown
