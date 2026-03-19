2938d67fc84e783361b40243b1d785b8892a67ec
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-03-19 10:56:49 +0100

    horarium: Add a TODO

--- Ubuntu/RootDotfiles/daily_schutdown_schedule/usr/local/bin/notify_and_shutdown.sh
+++ Ubuntu/RootDotfiles/daily_schutdown_schedule/usr/local/bin/notify_and_shutdown.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# TODO fix the notify-send https://www.perplexity.ai/search/notify-send-does-not-work-when-iCt5Y1qiSW.ZRYifyqzTug
+
 /usr/bin/notify-send --transient --urgency critical "Shutting down in 1 min" "$1"
 /sbin/shutdown
