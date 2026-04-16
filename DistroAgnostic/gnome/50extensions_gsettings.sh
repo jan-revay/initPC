a05a415a400e26f1c1d40223df0d70982703b39b
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-04-16 10:54:38 +0200

    GNOME settings - tilingshell: Add a TODO

--- DistroAgnostic/gnome/50extensions_gsettings.sh
+++ DistroAgnostic/gnome/50extensions_gsettings.sh
@@ -164,7 +164,7 @@ BASH
 fi # Ubuntu 24.04 CI
 
 # TODO configure Tiling shell in detail & in congruence with other keybindings
-# and settings.
+# and settings. https://github.com/domferr/tilingshell
 # tilingshell@ferrarodomenico.com
 for_each "gsettings set org.gnome.shell.extensions.tilingshell " << 'BASH'
     enable-move-keybindings "false"
