a6b67571123c6b665c4b5788d0dc0cfa1b174635
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-04-16 11:02:44 +0200

    GNOME keybindings: Add lower keybinding

--- DistroAgnostic/gnome/40keybindings_gsettings.sh
+++ DistroAgnostic/gnome/40keybindings_gsettings.sh
@@ -30,6 +30,7 @@ for_each "gsettings set org.gnome.desktop.wm.keybindings " << 'BASH'
     # TODO rethink workspace switching keybindings
     # TODO add home row keybindings for workspaces 9 to 12
     always-on-top "['<Control><Super>space']"
+    lower "['<Control><Alt>space']"
     move-to-workspace-1 "['<Control><Alt><Super>h', '<Alt><Shift>1', '<Control><Alt><Super>Home']"
     move-to-workspace-2 "['<Control><Alt><Super>j', '<Alt><Shift>2']"
     move-to-workspace-3 "['<Control><Alt><Super>k', '<Alt><Shift>3']"
