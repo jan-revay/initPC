5c1aae59cf1e507a90e56c3fae6562cacd377679
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-04-28 21:11:23 +0200

    run_all.sh: Add a TODO

--- DistroAgnostic/run_all.sh
+++ DistroAgnostic/run_all.sh
@@ -2,6 +2,10 @@
 . ../prelude.sh
 # This script should be idempotent.
 
+# TODO turn off logging for prelude, rename files with no dot or add regex and
+# use run-parts to run the scripts
+# TODO TOREAD https://www.reddit.com/r/linuxquestions/comments/1s3i5lb/do_you_actually_use_bash_for_your_scripts_or_just/
+
 # Run stuff that requires user input last (if not turned off by `--noninteractive`)
 for script in [0-9][0-9]*.sh; do
     [ -e "$script" ] || continue # skip if no match
