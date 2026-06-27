78030d64a4ccaa403efb109cf8461f8e42908a4e
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-06-27 16:00:08 +0200

    mkdir.sh: Sort alphabetically

--- DistroAgnostic/40mkdirs.sh
+++ DistroAgnostic/40mkdirs.sh
@@ -2,10 +2,10 @@
 . ../prelude.sh
 # The script should not require any user input and should be idempotent.
 
-mkdir -p ~/books
-mkdir -p ~/x     # all my development repos go here
-mkdir -p ~/x/tmp # stuff that can be deleted after restart
 # localSW is also created in source_packages.sh (they are installed there)
+mkdir -p ~/Documents
+mkdir -p ~/books
 mkdir -p ~/localSW
 mkdir -p ~/tmp
-mkdir -p ~/Documents
+mkdir -p ~/x     # all my development repos go here
+mkdir -p ~/x/tmp # stuff that can be deleted after restart
