9e658f9cc538c72577e936b0c7464756ccdae9a4
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-08-25 19:46:15 +0200

    Flatpaks and snaps: Refactor and comments cleanup

--- DistroAgnostic/gnome/10flatpaks_snaps_appimages_nix.sh
+++ DistroAgnostic/gnome/10flatpaks_snaps_appimages_nix.sh
@@ -3,103 +3,91 @@
 
 if [[ -v SKIP_FLATS_AND_SNAPS && "${SKIP_FLATS_AND_SNAPS}" == 'true' ]]; then
     echo 'Skipping flatpaks and snaps as SKIP_FLATS_AND_SNAPS parameter was provided...'
-else
-
-    # TODO consider running this in a separate github runner so that I will not risk
-    # resource exhaustion.
-
-    # TODO setup automatic flatpak and snap updates
-    # TODO skip install if running inside a docker container and print warning
+elif [ -f /.dockerenv ]; then
     # TODO print the text in color and review the code for Docker skipping.
     # TODO maybe the way of detecting docker is not great (try improving)
-    if [ -f /.dockerenv ]; then
-        echo "The initPC script is running inside a Docker container"
-        echo "Skipping installation of flatpaks/snaps/appimages/nix packages"
-    else
-        echo "The initPC script is not running inside Docker"
-        echo "Installing flatpaks/snaps/appimages/nix packages"
+    echo "The initPC script is running inside a Docker container"
+    echo "Skipping installation of flatpaks/snaps/appimages/nix packages"
+else
+    # TODO setup automatic flatpak and snap updates
+    echo "The initPC script is not running inside Docker"
+    echo "Installing flatpaks/snaps/appimages/nix packages"
 
-        # TODO add Nix package manager & nix packages
-        # maybe Nix packages can be used in Docker? TODO
+    # TODO add Nix package manager & nix packages
+    # maybe Nix packages can be used in Docker? TODO
 
-        # TODO decide between flatpaks and snaps (or combine them)
-        # TODO VSC flatpak appears to be broken and lower quality than the snap
-        # TODO install VSC from a .deb package directly downloaded via website
-        # or find other vay to install it via .deb
-        # TODO test the flatpaks
+    # TODO VSC flatpak appears to be broken and lower quality than the snap
+    # TODO test the flatpaks
 
-        # TODO - is this needed?
-        sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    # TODO - is this needed?
+    sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-        flatpak update --assumeyes
-        # TODO also update snaps
+    flatpak update --assumeyes
+    # TODO also update snaps
 
-        # TODO toread https://man7.org/linux/man-pages/man1/flatpak-install.1.html
-        readonly FLATPAK_PACKAGES=(
-            # com.visualstudio.code - containerised VSC sucks for C++ developemtn -> install deb instead
-            com.github.johnfactotum.Foliate
-            org.signal.Signal
-            # TODO try Caprine out and remove if not useful
-            com.sindresorhus.Caprine
-            com.spotify.Client
-            com.axosoft.GitKraken
-            com.obsproject.Studio
-            com.obsproject.Studio.Plugin.WebSocket
-            com.obsproject.Studio.Plugin.SourceClone
-            com.github.wwmm.easyeffects
-            net.werwolv.ImHex
-            io.github.dimtpap.coppwr
+    # TODO toread https://man7.org/linux/man-pages/man1/flatpak-install.1.html
+    readonly FLATPAK_PACKAGES=(
+        # com.visualstudio.code - containerised VSC sucks for C++ developemtn -> install deb instead
+        com.github.johnfactotum.Foliate
+        org.signal.Signal
+        # TODO try Caprine out and remove if not useful
+        com.sindresorhus.Caprine
+        com.spotify.Client
+        com.axosoft.GitKraken
+        com.obsproject.Studio
+        com.obsproject.Studio.Plugin.WebSocket
+        com.obsproject.Studio.Plugin.SourceClone
+        com.github.wwmm.easyeffects
+        net.werwolv.ImHex
+        io.github.dimtpap.coppwr
 
-            # benchmarks and stress tests
-            com.geeks3d.furmark
-            com.geekbench.Geekbench6
-            app.zen_browser.zen
-            no.mifi.losslesscut
-        )
-        # TODO try fixing flatpak installation without sudo (or at least do some research
-        # whether the sudo will not break sth.)
-        time sudo flatpak install --noninteractive --assumeyes flathub "${FLATPAK_PACKAGES[@]}"
+        # benchmarks and stress tests
+        com.geeks3d.furmark
+        com.geekbench.Geekbench6
+        app.zen_browser.zen
+        no.mifi.losslesscut
+    )
+    # TODO try fixing flatpak installation without sudo (or at least do some research
+    # whether the sudo will not break sth.)
+    time sudo flatpak install --noninteractive --assumeyes flathub "${FLATPAK_PACKAGES[@]}"
 
-        # TODO verify Installing snaps from a list is not idempotent for some reason TODO try fixing
-        # TODO reloading bashrc might be needed (SNAP)
-        # list install is not idempotent for some reason - TODO fix
-        # TODO benchmark the performance of snap vs. cargo
-        # rust packages and maybe switch back to cargo
-        # but install specific versions so that the script
-        # will not break easily.
-        # TODO - consider using for_each here also
-        readonly SNAP_PACKAGES=(
-            anki-desktop # TODO maybe install directly via website (most up to date)
-            core
-            snap-store
-            todoist
-            onlyoffice-desktopeditors
-            organize-my-files
+    # TODO verify Installing snaps from a list is not idempotent for some reason TODO try fixing
+    # TODO reloading bashrc might be needed (SNAP)
+    # list install is not idempotent for some reason - TODO fix
+    # TODO benchmark the performance of snap vs. cargo
+    # rust packages and maybe switch back to cargo
+    # but install specific versions so that the script
+    # will not break easily.
+    readonly SNAP_PACKAGES=(
+        anki-desktop # TODO maybe install directly via website (most up to date)
+        core
+        snap-store
+        todoist
+        onlyoffice-desktopeditors
+        organize-my-files
 
-            # TODO add the CLI packages to UbuntuCLI (but use different install method)
-            dust
-            procs
-            onefetch
-            drive
-            firefox
-            chromium
-            chromium-ffmpeg
-        )
-        readonly SNAP_CLASSIC_PACKAGES=(
-            helix # TODO move to UbuntuCLI
-            gitkraken
-            clion
-            go
-            code
-        )
-        time sudo snap refresh
-        time print0 "${SNAP_PACKAGES[@]}" \
-            | xargs -0 -I % sudo snap install %
-        time print0 "${SNAP_CLASSIC_PACKAGES[@]}" \
-            | xargs -0 -I % sudo snap install --classic %
+        # TODO add the CLI packages to UbuntuCLI (but use different install method)
+        dust
+        procs
+        onefetch
+        drive
+        firefox
+        chromium
+        chromium-ffmpeg
+    )
+    readonly SNAP_CLASSIC_PACKAGES=(
+        helix # TODO move to UbuntuCLI
+        gitkraken
+        clion
+        go
+        code
+    )
+    time sudo snap refresh
+    time print0 "${SNAP_PACKAGES[@]}" \
+        | xargs -0 -I % sudo snap install %
+    time print0 "${SNAP_CLASSIC_PACKAGES[@]}" \
+        | xargs -0 -I % sudo snap install --classic %
 
-        sudo snap install --edge yt-dlp
-        sudo snap refresh --edge yt-dlp
-    fi
-
-fi # SKIP_FLATS_AND_SNAPS
+    sudo snap install --edge yt-dlp
+    sudo snap refresh --edge yt-dlp
+fi
