eb138158e0994ed39a06d20f5ec8148c022d1ea2
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-08-26 14:11:13 +0200

    50install_kitty.sh: Add a TODO

--- Ubuntu/50install_kitty.sh
+++ Ubuntu/50install_kitty.sh
@@ -2,6 +2,11 @@
 . ../prelude.sh
 # The script should not require any user input and should be idempotent.
 
+# TODO - also check dependencies (glibc)
+# strings ~/.local/kitty.app/bin/kitty | grep -o 'GLIBC_[0-9.]*' | sort -V | tail -1
+# ldd --version
+# glibc kitty was built against needs to be older than the current glibc
+# also check other dependencies
 # TODO - could this cause compatibility issues (e.g. glibc ? - do research)
 install_kitty()
 {
