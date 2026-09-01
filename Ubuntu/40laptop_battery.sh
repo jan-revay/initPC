4a2d5dd3543e627a9a085b842ccd4fa22e2406b3
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-09-01 15:14:07 +0200

    Ubuntu - laptop battery: Add env var for max charge

--- Ubuntu/40laptop_battery.sh
+++ Ubuntu/40laptop_battery.sh
@@ -3,6 +3,11 @@
 
 # If on my work ThinkPad P1 Gen8
 if lscpu | grep -F "Intel(R) Core(TM) Ultra 7 265H"; then
-    echo 70 | sudo tee /sys/class/power_supply/BAT0/charge_control_start_threshold
-    echo 80 | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold
+    if [[ -v ST0_BATTERY_MAX_CHARGE && "${ST0_BATTERY_MAX_CHARGE}" == "true" ]]; then
+        echo 0 | sudo tee /sys/class/power_supply/BAT0/charge_control_start_threshold
+        echo 100 | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold
+    else
+        echo 70 | sudo tee /sys/class/power_supply/BAT0/charge_control_start_threshold
+        echo 80 | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold
+    fi
 fi
