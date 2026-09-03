46e26833ba146c70745013164e84a9bf83e981cc
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-09-03 17:40:50 +0200

    CLI_Ubuntu: Add a TODO

--- CLI_Ubuntu/run_all.sh
+++ CLI_Ubuntu/run_all.sh
@@ -8,6 +8,10 @@
 # TODO do pushd and popd here so that the runner can be
 # triggered from anywhere (compute absolute path to the
 # script) ??
+
+# TODO - add a flag to ignore distro and distro distro version
+# so that I can run this script on Debian and Kali containers
+# for CI & testing
 if distro_is ubuntu && distro_version_ge 24; then
     # stuff that requires user input can be turned off by `--noninteractive` CLI param
     for script in [0-9][0-9]*.sh; do
