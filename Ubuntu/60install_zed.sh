f6efd9841ac6f1a28b01a50c9c10d20c4d8c70e3
JohnnyRevay <65253638+jan-revay@users.noreply.github.com>
2026-09-13 15:44:55 +0200

    Zed: Add a TODO

--- Ubuntu/60install_zed.sh
+++ Ubuntu/60install_zed.sh
@@ -2,6 +2,8 @@
 . ../prelude.sh
 # The script should not require any user input and should be idempotent.
 
+# TODO consider installing it via homebrew instead
+
 # TODO - also check dependencies (glibc)
 # strings ~/.local/kitty.app/bin/kitty | grep -o 'GLIBC_[0-9.]*' | sort -V | tail -1
 # ldd --version
